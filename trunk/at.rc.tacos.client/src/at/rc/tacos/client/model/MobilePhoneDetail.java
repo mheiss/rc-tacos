2271c05d2514a70b90525567428a1d716ef7b4f5
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-10-26 21:32:24 +0000

    initialising

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/model/MobilePhoneDetail.java
@@ -0,0 +1,72 @@
+/**
+ * 
+ * @author b.thek
+ */
+
+package at.rc.tacos.client.model;
+
+
+public class MobilePhoneDetail 
+{
+
+	private String mobilePhoneId;//Muss von Art: z.B. 'Bm01' sein (Zweistelliges Ortsstellen-Kennzeichen, zweistellige Nummer)- zwecks Vergleich mit zugehörigem Fahrzeug
+	private String mobilePhoneNumber;
+	
+	/**
+	 * Constructors
+	 */
+	public MobilePhoneDetail()
+	{
+		
+	}
+
+	/**
+	 * @param mobilePhoneId
+	 * @param mobilphonenumer
+	 */
+	public MobilePhoneDetail(String mobilePhoneId, String mobilePhoneNumber) {
+		super();
+		this.mobilePhoneId = mobilePhoneId;
+		this.mobilePhoneNumber = mobilePhoneNumber;
+	}
+
+	
+	/**
+	 * Setter&Getter
+	 */
+	
+	
+	/**
+	 * @return the mobilePhoneId
+	 */
+	public String getMobilePhoneId() {
+		return mobilePhoneId;
+	}
+
+	/**
+	 * @param mobilePhoneId the mobilePhoneId to set
+	 */
+	public void setMobilePhoneId(String mobilePhoneId) {
+		this.mobilePhoneId = mobilePhoneId;
+	}
+
+	/**
+	 * @return the mobilePhoneNumber
+	 */
+	public String getMobilePhoneNumber() {
+		return mobilePhoneNumber;
+	}
+
+	/**
+	 * @param mobilphonenumer the mobilePhoneNumber to set
+	 */
+	public void setMobilePhoneNumber(String mobilePhoneNumber) {
+		this.mobilePhoneNumber = mobilePhoneNumber;
+	}
+	
+	
+	
+	
+	
+}
+
