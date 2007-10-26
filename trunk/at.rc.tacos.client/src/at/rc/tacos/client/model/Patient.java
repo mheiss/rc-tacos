8e63295417da273828856cc6579de6d22b8f16dd
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-10-26 21:56:55 +0000

    initialization

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/model/Patient.java
@@ -0,0 +1,78 @@
+package at.rc.tacos.client.model;
+
+/**
+ * Contains the data of the patient
+ * Will be extended if a patient database is implemented
+ * @author b.thek
+ */
+
+public class Patient 
+{
+	private long patientId;
+	private String firstname;
+	private String lastname;
+	
+	/**
+	 * Constructors
+	 */
+	public Patient(){}
+
+	/**
+	 * @param patientId
+	 * @param firstname
+	 * @param lastname
+	 */
+	public Patient(long patientId, String firstname, String lastname) {
+		super();
+		this.patientId = patientId;
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+
+	
+	/**
+	 * Getter&Setter
+	 */
+	
+	/**
+	 * @return the patientId
+	 */
+	public long getPatientId() {
+		return patientId;
+	}
+
+	/**
+	 * @param patientId the patientId to set
+	 */
+	public void setPatientId(long patientId) {
+		this.patientId = patientId;
+	}
+
+	/**
+	 * @return the firstname
+	 */
+	public String getFirstname() {
+		return firstname;
+	}
+
+	/**
+	 * @param firstname the firstname to set
+	 */
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	/**
+	 * @return the lastname
+	 */
+	public String getLastname() {
+		return lastname;
+	}
+
+	/**
+	 * @param lastname the lastname to set
+	 */
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}	
+}
