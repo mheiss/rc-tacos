d857e2430d21ee20e2094a67d9d908b9840cbd70
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-11-04 21:17:28 +0000

    initialization

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/model/ITransportPriority.java
@@ -0,0 +1,18 @@
+package at.rc.tacos.client.model;
+
+/**
+ * The available priorities for a transport
+ * @author b.thek
+ */
+public interface ITransportPriority 
+{
+	/** Priority of transport*/
+	public final static String TRANSPORT_PRIORITY_EMERGENCY_DOCTOR_INTERNAL = "A";	//NEF und RTW entsenden
+	public final static String TRANSPORT_PRIORITY_BLUELIGHT = "B";					//RTW mit BD1 entsenden
+	public final static String TRANSPORT_PRIORITY_TRANSPORT = "C";					//normaler Transport
+	public final static String TRANSPORT_PRIORITY_BACK_TRANSPORT = "D";				//Rücktransport (von ambulant)
+	public final static String TRANSPORT_PRIORITY_HOME_TRANSPORT = "E";				//Heimtransport (von stationär)
+	public final static String TRANSPORT_PRIORITY_OTHER = "F";						//Sonstiges (Dienstfahrten,...)
+	public final static String TRANSPORT_PRIORITY_EMERGENCY_DOCTOR_EXTERNAL = "G";	//NEF für andere Bezirksstelle entsenden
+}
+
