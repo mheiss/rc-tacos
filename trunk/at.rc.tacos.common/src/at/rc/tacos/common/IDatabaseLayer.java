1fbdbe07b43a5cff9e8b83a2e954e74f838be883
heissm <michael.heiss@outlook.at>
2007-11-06 17:54:08 +0000

    Initial import. The common plugin provides methods that are used by several plugins.

---
+++ trunk/at.rc.tacos.common/src/at/rc/tacos/common/IDatabaseLayer.java
@@ -0,0 +1,10 @@
+package at.rc.tacos.common;
+
+/**
+ * Defines methods for the implementation of the database.
+ * @author Michael
+ */
+public interface IDatabaseLayer
+{
+	public void queryItem();
+}
---
+++ trunk/at.rc.tacos.common/src/at/rc/tacos/common/INetLayer.java
@@ -0,0 +1,13 @@
+package at.rc.tacos.common;
+
+/**
+ * Defines methods for the implementation of the network protocol.
+ * @author Michael
+ */
+public interface INetLayer
+{
+    /**
+     * This method is called when the item was changed
+     */
+    public void itemChanged(String newItem);
+}
