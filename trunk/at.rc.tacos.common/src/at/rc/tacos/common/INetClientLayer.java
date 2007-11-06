56feff4b9572cf0210df86e10dba41f00304a149
heissm <heissm@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-11-06 21:49:04 +0000

    Implemented a pre aplha network communication

---
+++ trunk/at.rc.tacos.common/src/at/rc/tacos/common/INetClientLayer.java
@@ -0,0 +1,11 @@
+package at.rc.tacos.common;
+
+/**
+ * Defines methods for the implementation of the network protocol.<br>
+ * This methods are designed update the client ui.
+ * @author Michael
+ */
+public interface INetClientLayer
+{
+    public void itemAdded(String item);
+}
