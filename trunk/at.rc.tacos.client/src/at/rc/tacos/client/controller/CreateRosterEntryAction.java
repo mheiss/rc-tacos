f5a725d5b59448a6dafc0ab64e80490a3362206e
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-11-11 03:51:07 +0000

    Initialization

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/controller/CreateRosterEntryAction.java
@@ -0,0 +1,75 @@
+package at.rc.tacos.client.controller;
+
+//rcp
+import java.util.Date;
+
+import org.eclipse.jface.action.Action;
+//client
+import at.rc.tacos.core.model.*;
+import at.rc.tacos.core.service.ServiceWrapper;
+
+/**
+ * The action that is triggered by creating a new roster entry
+ * @author b.thek
+ */
+public class CreateRosterEntryAction extends Action 
+{
+	private long rosterId;
+	private StaffMember staffMember;
+	private Date dateOfRosterEntry;
+	private long plannedStartofWork;
+	private long plannedEndOfWork;
+	private long realStartOfWork;
+	private long realEndOfWork;
+	private String station;
+	private String competence;
+	private String servicetype;
+	private String rosterNotes;
+	private boolean standby;
+
+    /**
+	 * @param rosterId
+	 * @param staffMember
+	 * @param dateOfRosterEntry
+	 * @param plannedStartofWork
+	 * @param plannedEndOfWork
+	 * @param realStartOfWork
+	 * @param realEndOfWork
+	 * @param station
+	 * @param competence
+	 * @param servicetype
+	 * @param rosterNotes
+	 * @param standby
+	 */
+	public CreateRosterEntryAction(long rosterId, StaffMember staffMember,
+			Date dateOfRosterEntry, long plannedStartofWork,
+			long plannedEndOfWork, long realStartOfWork, long realEndOfWork,
+			String station, String competence, String servicetype,
+			String rosterNotes, boolean standby) 
+	{
+		
+		this.rosterId = rosterId;
+		this.staffMember = staffMember;
+		this.dateOfRosterEntry = dateOfRosterEntry;
+		this.plannedStartofWork = plannedStartofWork;
+		this.plannedEndOfWork = plannedEndOfWork;
+		this.realStartOfWork = realStartOfWork;
+		this.realEndOfWork = realEndOfWork;
+		this.station = station;
+		this.competence = competence;
+		this.servicetype = servicetype;
+		this.rosterNotes = rosterNotes;
+		this.standby = standby;
+	}
+    
+    public void run() 
+    {
+    	RosterEntry newRosterEntry = new RosterEntry(rosterId,staffMember,dateOfRosterEntry,
+    			plannedStartofWork, plannedEndOfWork,
+    			realStartOfWork, realEndOfWork, station,
+    			competence, servicetype, rosterNotes,
+    			standby);
+    	//ServiceWrapper.getDefault().getNetService().requestAddRosterEntry(newRosterEntry.get???(item)-------?
+
+    }
+}
