b9b909d1b2f09cdca93e47e1c5bbf2d7967ee763
heissm <heissm@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-11-12 19:02:16 +0000

    Implemented the serialization and deserialization of the object item. Moved the package at.rc.tacos.core.model to at.rc.tacos.model (own plugin) Updated the xml and net packages

--- trunk/at.rc.tacos.common/src/at/rc/tacos/common/IDatabaseLayer.java
+++ trunk/at.rc.tacos.common/src/at/rc/tacos/common/IDatabaseLayer.java
@@ -1,7 +1,8 @@
 package at.rc.tacos.common;
 
 /**
- * Defines methods for the implementation of the database.
+ * This interface is designed to offer the server the 
+ * chance to access the database layer throught the service layer.
  * @author Michael
  */
 public interface IDatabaseLayer
--- trunk/at.rc.tacos.common/src/at/rc/tacos/common/IXMLObject.java
+++ trunk/at.rc.tacos.common/src/at/rc/tacos/common/IXMLObject.java
@@ -5,14 +5,14 @@ import javax.xml.stream.XMLStreamWriter;
 
 /**
  * Methods must implement this interface so that the can 
- * be serialized to xml.
+ * be serialized to xml and back.
  * @author Michael
  */
-public interface IXMLSerialize
-{
+public interface IXMLObject
+{   
     /** Serialize to xml */
     public void toXML(XMLStreamWriter writer);
     
     /** Deserialize from xml */
-    public IXMLSerialize toObject(XMLEventReader reader);
+    public IXMLObject toObject(XMLEventReader reader);
 }
