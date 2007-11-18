adba5da940a834c7730e72b3d2af1c956f2fdec9
heissm <michael.heiss@outlook.at>
2007-11-18 14:11:39 +0000

    Bufix for the xml codec.

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/modelManager/RosterEntryManager.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/modelManager/RosterEntryManager.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.List;
 //rcp
 import org.eclipse.core.runtime.PlatformObject;
+import org.eclipse.swt.widgets.Display;
 //model
 import at.rc.tacos.model.*;
 
@@ -17,80 +18,92 @@ import at.rc.tacos.model.*;
  */
 public class RosterEntryManager extends PlatformObject 
 {
-	//the item list
-	private List<RosterEntry> objectList = new ArrayList<RosterEntry>();
+    //the item list
+    private List<RosterEntry> objectList = new ArrayList<RosterEntry>();
 
-	//the listeners to inform about data changes
-	protected transient PropertyChangeSupport listeners = new PropertyChangeSupport(this);
+    //the listeners to inform about data changes
+    protected transient PropertyChangeSupport listeners = new PropertyChangeSupport(this);
 
-	/**
-	 * Adds a property-change listener.
-	 * @param l the listener
-	 */
-	public void addPropertyChangeListener(PropertyChangeListener l)
-	{
-		if (l == null) 
-			throw new IllegalArgumentException();
-		listeners.addPropertyChangeListener(l);
-	}
+    /**
+     * Adds a property-change listener.
+     * @param l the listener
+     */
+    public void addPropertyChangeListener(PropertyChangeListener l)
+    {
+        if (l == null) 
+            throw new IllegalArgumentException();
+        listeners.addPropertyChangeListener(l);
+    }
 
-	public void removePropertyChangeListener(PropertyChangeListener l)
-	{
-		listeners.removePropertyChangeListener(l);
-	}
+    public void removePropertyChangeListener(PropertyChangeListener l)
+    {
+        listeners.removePropertyChangeListener(l);
+    }
 
-	/**
-	 * Notificates all listeners to a model-change
-	 * @param prop the property-id
-	 * @param old the old-value
-	 * @param newValue the new value
-	 */
-	protected void firePropertyChange(String prop, Object old, Object newValue)
-	{
-		if (listeners.hasListeners(prop)) 
-			listeners.firePropertyChange(prop, old, newValue);
-	}
+    /**
+     * Notificates all listeners to a model-change
+     * @param prop the property-id
+     * @param old the old-value
+     * @param newValue the new value
+     */
+    protected void firePropertyChange(String prop, Object old, Object newValue)
+    {
+        if (listeners.hasListeners(prop)) 
+            listeners.firePropertyChange(prop, old, newValue);
+    }
 
-	/**
-	 * Adds a new roster entry to the list
-	 * @param roster entry the roster entry to add
-	 */
-	public void add(RosterEntry rosterEntry) 
-	{
-		//add the item
-		objectList.add(rosterEntry);
-		//notify the view
-		firePropertyChange("ROSTERENTRY_ADD", null, rosterEntry);
-	}    
+    /**
+     * Adds a new roster entry to the list
+     * @param roster entry the roster entry to add
+     */
+    public void add(final RosterEntry rosterEntry) 
+    {
+        Display.getDefault().syncExec(new Runnable ()    
+        {
+            public void run ()       
+            {
+                //add the item
+                objectList.add(rosterEntry);
+                //notify the view
+                firePropertyChange("ROSTERENTRY_ADD", null, rosterEntry);
+            }
+        }); 
+    }    
 
-	/**
-	 * Removes the roster entry from the list
-	 * @param roster entry the roster entry to remove
-	 */
-	public void remove(RosterEntry rosterEntry) 
-	{
-		objectList.remove(rosterEntry);
-		firePropertyChange("ROSTERENTRY_REMOVE", rosterEntry, null); 
-	}
+    /**
+     * Removes the roster entry from the list
+     * @param roster entry the roster entry to remove
+     */
+    public void remove(final RosterEntry rosterEntry) 
+    {
+        Display.getDefault().syncExec(new Runnable ()    
+        {
+            public void run ()       
+            {
+                objectList.remove(rosterEntry);
+                firePropertyChange("ROSTERENTRY_REMOVE", rosterEntry, null); 
+            }
+        }); 
+    }
 
-	/**
-	 * Converts the list to an array
-	 * @return the list as a array
-	 */
-	public Object[] toArray()
-	{
-		return objectList.toArray();
-	}
+    /**
+     * Converts the list to an array
+     * @return the list as a array
+     */
+    public Object[] toArray()
+    {
+        return objectList.toArray();
+    }
 
-	public Object[] toArray(String station)
-	{
-		List<RosterEntry> filteredList = new ArrayList<RosterEntry>();
-		for(RosterEntry entry:objectList)
-		{
-			if (entry.getStation().equalsIgnoreCase(station))
-				filteredList.add(entry);
-		}
-		return filteredList.toArray();
-	}
+    public Object[] toArray(String station)
+    {
+        List<RosterEntry> filteredList = new ArrayList<RosterEntry>();
+        for(RosterEntry entry:objectList)
+        {
+            if (entry.getStation().equalsIgnoreCase(station))
+                filteredList.add(entry);
+        }
+        return filteredList.toArray();
+    }
 }
 
