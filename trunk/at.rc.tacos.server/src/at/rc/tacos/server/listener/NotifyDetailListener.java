5d9e3a05c28f012ef272d9478ac17900dce8fe40
heissm <heissm@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-12-04 02:58:13 +0000

    Database: Implemented a dao factory to manage the available daos. Database: Renamed and updated the layer objects Database: Created a dao test factory and provided test data.
    Server: Implemeted the logic to work with the dummy data

--- trunk/at.rc.tacos.server/src/at/rc/tacos/server/listener/NotifyDetailListener.java
+++ trunk/at.rc.tacos.server/src/at/rc/tacos/server/listener/NotifyDetailListener.java
@@ -1,10 +1,60 @@
 package at.rc.tacos.server.listener;
 
+import java.util.ArrayList;
+import at.rc.tacos.common.AbstractMessage;
+import at.rc.tacos.core.db.dao.CallerDAO;
+import at.rc.tacos.model.NotifierDetail;
+import at.rc.tacos.server.dao.DaoService;
+
 /**
  * This class will be notified uppon NotifyDetail changes
  * @author Michael
  */
 public class NotifyDetailListener extends ServerListenerAdapter
 {
-  
+    private CallerDAO callerDao = DaoService.getInstance().getFactory().createNotifierDAO();
+    
+    /**
+     * Add a notifier detail
+     */
+    @Override
+    public AbstractMessage handleAddRequest(AbstractMessage addObject)
+    {
+        NotifierDetail detail = (NotifierDetail)addObject;
+        callerDao.addCaller(detail);
+        return detail;
+    }
+
+    /**
+     * Listing of all notifiers
+     */
+    @Override
+    public ArrayList<AbstractMessage> handleListingRequest()
+    {
+        ArrayList<AbstractMessage> list = new ArrayList<AbstractMessage>();
+        list.addAll(callerDao.listCallers());
+        return list;
+    }
+
+    /**
+     * Remove a mobile phone
+     */
+    @Override
+    public AbstractMessage handleRemoveRequest(AbstractMessage removeObject)
+    {
+        NotifierDetail detail = (NotifierDetail)removeObject;
+        callerDao.removeCaller(detail);
+        return detail;
+    }
+
+    /**
+     * Update a notifier detail
+     */
+    @Override
+    public AbstractMessage handleUpdateRequest(AbstractMessage updateObject)
+    {
+        NotifierDetail detail = (NotifierDetail)updateObject;
+        callerDao.updateCaller(detail);
+        return detail;
+    }
 }
--- trunk/at.rc.tacos.server/src/at/rc/tacos/server/listener/VehicleDetailListener.java
+++ trunk/at.rc.tacos.server/src/at/rc/tacos/server/listener/VehicleDetailListener.java
@@ -1,10 +1,63 @@
 package at.rc.tacos.server.listener;
 
+import java.util.ArrayList;
+
+import at.rc.tacos.common.AbstractMessage;
+import at.rc.tacos.core.db.dao.VehicleDAO;
+import at.rc.tacos.core.db.dao.test.VehicleDetailDAOTest;
+import at.rc.tacos.model.VehicleDetail;
+import at.rc.tacos.server.dao.DaoService;
+
 /**
  * This class will be notified uppon vehicle detail changes
  * @author Michael
  */
 public class VehicleDetailListener extends ServerListenerAdapter
 {
- 
+    private VehicleDAO vehicleDao = DaoService.getInstance().getFactory().createVehicleDetailDAO();
+    
+    /**
+     * Vehicle added
+     */
+    @Override
+    public AbstractMessage handleAddRequest(AbstractMessage addObject)
+    {
+        VehicleDetail vehicle = (VehicleDetail)addObject;
+        int id = vehicleDao.addVehicle(vehicle);
+        vehicle.setVehicleId(id);
+        return vehicle;
+    }
+
+    /**
+     * Vehicle listing
+     */
+    @Override
+    public ArrayList<AbstractMessage> handleListingRequest()
+    {
+        ArrayList<AbstractMessage> list = new ArrayList<AbstractMessage>();
+        list.addAll(vehicleDao.listVehicles());
+        return list;
+    }
+
+    /**
+     * Remove a vehicle
+     */
+    @Override
+    public AbstractMessage handleRemoveRequest(AbstractMessage removeObject)
+    {
+        VehicleDetail vehicle = (VehicleDetail)removeObject;
+        vehicleDao.removeVehicle(vehicle);
+        return vehicle;
+    }
+
+    /**
+     * Updates a vehicle
+     */
+    @Override
+    public AbstractMessage handleUpdateRequest(AbstractMessage updateObject)
+    {
+        VehicleDetail vehicle = (VehicleDetail)updateObject;
+        vehicleDao.updateVehicle(vehicle);
+        return vehicle;
+    }
 }
