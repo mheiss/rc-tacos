b4eac438eb87348888c49d8811269547112d8262
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-12-16 21:50:08 +0000

    jobs added

--- trunk/at.rc.tacos.model/src/at/rc/tacos/common/Constants.java
+++ trunk/at.rc.tacos.model/src/at/rc/tacos/common/Constants.java
@@ -35,6 +35,20 @@ public class Constants
     public final static String SERVICE_OTHER = "Sonstiges";
     public final static String SERVICE_TEMP = "Ersatzeinstellung";
     
+    //Job
+    public final static String JOB_DRIVER = "Fahrer";
+    public final static String JOB_SANI = "Sanitäter";
+    public final static String JOB_EMERGENCY = "Notfallsanitäter";
+    public final static String JOB_DOCTOR = "Notarzt";
+    public final static String JOB_DISPON = "Leitstellendisponent";
+    public final static String JOB_DF = "Dienstführender";
+    public final static String JOB_BRKDT = "Bezirksrettungskommandant";
+    public final static String JOB_INSP = "Inspektionsdienst";
+    public final static String JOB_BKTW_DRIVER = "BKTW- Fahrer";
+    public final static String JOB_JOURNAL = "Journaldienst";
+    public final static String JOB_VOLON = "Volontär";
+    public final static String JOB_OTHER = "Sonstiges";
+    
     /** Array of all available stations */
     public static String [] stations = { STATION_BEZIRK , STATION_KAPFENBERG , STATION_BRUCK ,
         STATION_MAREIN, STATION_THOERL , STATION_THOERL ,STATION_BREITENAU };
@@ -50,5 +64,10 @@ public class Constants
     public static String[] timeArray = { "05:00", "05:30", "06:00", "06:30", "07:00", "07:30", "08:00", "08:30", "09:00", 
         "09:30", "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00", "14:30", "15:00", "15:30", 
         "16:00", "16:30", "17:00", "17:30", "18:00", "18:30", "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", 
-        "22:30", "23:00", "23:30", "00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30"};   
+        "22:30", "23:00", "23:30", "00:00", "00:30", "01:00", "01:30", "02:00", "02:30", "03:00", "03:30", "04:00", "04:30"};  
+    
+    /** Array of all jobs */
+    public static String[] job = {JOB_DRIVER, JOB_SANI, JOB_EMERGENCY, JOB_DOCTOR, JOB_DISPON, JOB_DF, JOB_BRKDT, JOB_INSP, JOB_BKTW_DRIVER, 
+    	JOB_JOURNAL, JOB_VOLON, JOB_OTHER};
+    
 }
