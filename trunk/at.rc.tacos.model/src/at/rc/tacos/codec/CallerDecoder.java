d7296f814ea67b04f26006fe8e70e6cb8626d838
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-12-17 00:43:49 +0000

    Renamed

---
+++ trunk/at.rc.tacos.model/src/at/rc/tacos/codec/CallerDecoder.java
@@ -0,0 +1,50 @@
+package at.rc.tacos.codec;
+
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.XMLEvent;
+
+import at.rc.tacos.common.AbstractMessage;
+import at.rc.tacos.model.CallerDetail;
+
+public class CallerDecoder implements MessageDecoder
+{
+    @Override
+    public AbstractMessage doDecode(XMLEventReader reader) throws XMLStreamException
+    {
+        //Create a new notifier
+        CallerDetail notifier = new CallerDetail();
+            
+        //parse and set up the object
+        while(reader.hasNext())
+        {
+            //the type of the event
+            XMLEvent event = reader.nextEvent();
+            if (event.isStartElement()) 
+            {
+                String startName = event.asStartElement().getName().getLocalPart();
+                //create a new item 
+                if(CallerDetail.ID.equalsIgnoreCase(startName))
+                    notifier = new CallerDetail();
+                
+                //get the type of the element and set the corresponding value
+                if("name".equalsIgnoreCase(startName))
+                    notifier.setCallerName(reader.getElementText());
+                if("telephoneNumer".equalsIgnoreCase(startName))
+                    notifier.setCallerTelephoneNumber(reader.getElementText());
+                if("callerNotes".equalsIgnoreCase(startName))
+                    notifier.setCallerNotes(reader.getElementText());
+            }
+            //check for the end element, and return the object
+            if(event.isEndElement())
+            {
+                //get the name
+                String endElement = event.asEndElement().getName().getLocalPart();
+                //check if we have reached the end
+                if (CallerDetail.ID.equalsIgnoreCase(endElement))
+                    return notifier;
+            }
+        }
+        return null;
+    }
+}
