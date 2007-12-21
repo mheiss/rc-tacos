0b39632620adbd3de9280acd6c7e078a3cf455a3
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-12-21 19:20:01 +0000

    initialization

---
+++ trunk/at.rc.tacos.model/src/at/rc/tacos/common/IKindOfTransport.java
@@ -0,0 +1,14 @@
+package at.rc.tacos.common;
+
+public interface IKindOfTransport 
+{
+	/** gehend*/
+	public final static String TRANSPORT_KIND_GEHEND ="gehend";
+	/** Tragsessel*/
+	public final static String TRANSPORT_KIND_TRAGSESSEL ="Tragsessel";
+	/** Krankentrage*/
+	public final static String TRANSPORT_KIND_KRANKENTRAGE ="Krankentrage";
+	/** Eigener Rollstuhl*/
+	public final static String TRANSPORT_KIND_ROLLSTUHL ="Eigener Rollstuhl";
+		
+}
