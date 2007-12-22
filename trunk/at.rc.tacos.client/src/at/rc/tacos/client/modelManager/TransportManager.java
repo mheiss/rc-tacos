2c2430bbe948cb1e806cd05f49ee5e49d48e4b8f
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-12-22 04:15:09 +0000

    initialization

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/modelManager/TransportManager.java
@@ -0,0 +1,111 @@
+package at.rc.tacos.client.modelManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.eclipse.swt.widgets.Display;
+
+import at.rc.tacos.common.Constants;
+import at.rc.tacos.model.*;
+
+/**
+ * All transports
+ * @author b.thek
+ */
+public class TransportManager extends DataManager 
+{
+    //the item list
+    private List<Transport> objectList = new ArrayList<Transport>();
+    
+    //the active day that is showed
+    private String station;
+    
+    /**
+     * Default class constructor
+     */
+    public TransportManager()
+    {
+//        station = Constants.STATION_BEZIRK;
+    }
+
+    /**
+     * Adds a new transport to the list
+     * @param rosterEntry the transport to add
+     */
+    public void add(final Transport transport) 
+    {
+        Display.getDefault().syncExec(new Runnable ()    
+        {
+            public void run ()       
+            {
+                //add the item
+                objectList.add(transport);
+                //notify the view
+                firePropertyChange("TRANSPORT_ADD", null, transport);
+            }
+        }); 
+    }    
+
+    /**
+     * Removes the transport from the list
+     * @param rosterEntry the transport to remove
+     */
+    public void remove(final Transport transport) 
+    {
+        Display.getDefault().syncExec(new Runnable ()    
+        {
+            public void run ()       
+            {
+                objectList.remove(transport);
+                firePropertyChange("TRANSPORT_REMOVE", transport, null); 
+            }
+        }); 
+    }
+    
+    
+    /**
+     * Updates the transport at the list
+     * @param rosterEntry the transport to update
+     */
+    public void update(final Transport transport) 
+    {
+        Display.getDefault().syncExec(new Runnable ()    
+        {
+            public void run ()       
+            {  	
+            	//get the position of the entry
+            	int id = objectList.indexOf(transport);
+            	objectList.set(id, transport);
+                firePropertyChange("TRANSPORT_UPDATE", transport, null); 
+            }
+        }); 
+    }
+    
+    /**
+     * Removes all elements form the list
+     */
+    public void removeAllEntries()
+    {
+        Display.getDefault().syncExec(new Runnable ()    
+        {
+            public void run ()       
+            {   
+                objectList.clear();
+                firePropertyChange("TRANSPORT_CLEARED",null,null);
+            }
+        }); 
+        
+    }
+    
+
+    /**
+     * Converts the list to an array
+     * @return the list as a array
+     */
+    public Object[] toArray()
+    {
+        return objectList.toArray();
+    }
+
+}
+
+
