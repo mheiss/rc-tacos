f3a0f63ff8bf8a196c1f769bdfa03737dae4ec38
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-12-22 05:25:01 +0000

    initialization

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/controller/UpdateTransportAction.java
@@ -0,0 +1,22 @@
+package at.rc.tacos.client.controller;
+
+import org.eclipse.jface.action.Action;
+
+import at.rc.tacos.core.net.NetWrapper;
+import at.rc.tacos.model.Transport;
+
+public class UpdateTransportAction extends Action
+{
+	private Transport transport;
+
+	public UpdateTransportAction(Transport transport)
+	{
+		this.transport = transport;
+	}
+
+	@Override
+	public void run()
+	{
+		NetWrapper.getDefault().sendUpdateMessage(Transport.ID, transport);
+	}
+}
