29d416e2abd40b87827221abfd4c2444bc1c3eed
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-12-23 02:42:39 +0000

    init

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/controller/CancelTransportAction.java
@@ -0,0 +1,44 @@
+package at.rc.tacos.client.controller;
+
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+
+import at.rc.tacos.common.ITransportStatus;
+import at.rc.tacos.core.net.NetWrapper;
+import at.rc.tacos.model.Transport;
+
+/**
+ * Opens the editor to edit the selected entry
+ * @author b.thek
+ */
+public class CancelTransportAction extends Action implements ITransportStatus
+{
+	//properties
+	private TableViewer viewer;
+	
+	/**
+	 * Default class construtor.
+	 * @param viewer the table viewer
+	 */
+	public CancelTransportAction(TableViewer viewer)
+	{
+		this.viewer = viewer;
+		setText("Transport stornieren");
+		setToolTipText("Storniert den Transport und verschiebt ihn in das Journalblatt");
+	}
+	
+	@Override
+	public void run()
+	{
+		//the selection
+		ISelection selection = viewer.getSelection();
+		//get the selected transport
+		Transport transport = (Transport)((IStructuredSelection)selection).getFirstElement();
+		
+		transport.setTransportNumber("STORNO");
+		NetWrapper.getDefault().sendUpdateMessage(Transport.ID, transport);
+	}
+}
