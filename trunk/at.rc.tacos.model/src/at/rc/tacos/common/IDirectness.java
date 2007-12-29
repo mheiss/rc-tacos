67a449c7dd0011757be62207d73a8a74d586bb8b
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-12-29 18:37:15 +0000

    instead of bezirk bruck or kapfenberg

--- trunk/at.rc.tacos.model/src/at/rc/tacos/common/IDirectness.java
+++ trunk/at.rc.tacos.model/src/at/rc/tacos/common/IDirectness.java
@@ -9,7 +9,7 @@ public interface IDirectness
 {
 
 	/** Richtung Bezirk (default, wenn nichts anderes angegeben*/
-	public final static int TOWARDS_DISTRICT = 1;	
+	public final static int TOWARDS_BRUCK = 1;	
 	/** Richtung Graz */
 	public final static int TOWARDS_GRAZ = 2;	
 	/** Richtung Leoben */
@@ -18,5 +18,7 @@ public interface IDirectness
 	public final static int TOWARDS_VIENNA = 4;		
 	/** Richtung Mariazell*/
 	public final static int TOWARDS_MARIAZELL = 5;
+	/** Richtung Mariazell*/
+	public final static int TOWARDS_KAPFENBERG = 5;
 		
 }
