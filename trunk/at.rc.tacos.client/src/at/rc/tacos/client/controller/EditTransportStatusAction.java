7cb7d03f6a133f98526d63acef6c382c2a714fec
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2007-12-29 21:28:03 +0000

    init

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/controller/EditTransportStatusAction.java
@@ -0,0 +1,43 @@
+package at.rc.tacos.client.controller;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+
+import at.rc.tacos.client.view.TransportForm;
+import at.rc.tacos.client.view.TransportStatiForm;
+import at.rc.tacos.model.Transport;
+
+/**
+ * Opens the form to edit the transport stati
+ * @author b.thek
+ */
+public class EditTransportStatusAction extends Action
+{
+	//properties
+	private TableViewer viewer;
+	
+	/**
+	 * Default class constructor.
+	 * @param viewer the table viewer
+	 */
+	public EditTransportStatusAction(TableViewer viewer)
+	{
+		this.viewer = viewer;
+		setText("Stati bearbeiten");
+		setToolTipText("Öffnet ein Fenster um die Stati zu bearbeiten");
+	}
+	
+	@Override
+	public void run()
+	{
+		//the selection
+		ISelection selection = viewer.getSelection();
+		//get the selected transport
+		Transport transport = (Transport)((IStructuredSelection)selection).getFirstElement();
+		//open the editor
+		TransportStatiForm form = new TransportStatiForm(transport);
+//		form.open();
+	}
+}
