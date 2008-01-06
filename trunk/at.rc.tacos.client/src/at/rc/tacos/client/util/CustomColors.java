52015ff7bb5571d11a14d9b314d688c8e0e877ce
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-01-06 04:30:45 +0000

    dark grey rgb value changed

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/util/CustomColors.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/util/CustomColors.java
@@ -17,10 +17,10 @@ public final class CustomColors
     public static final Font SUBHEADER_FONT = new Font(null,"Arial", 10, SWT.BOLD);
     /** Set the main color scheme */
     public static final Color RED_COLOR = new Color(null,186,15,23);
-    public static final Color GREY_COLOR = new Color(null,236,233,226);
+    public static final Color GREY_COLOR = new Color(null,192,192,192);
     public static final Color HEADING_COLOR = new Color(null,255,255,255);
     public static final Color SECTION_BACKGROUND = new Color(null,255,255,255);
-    public static final Color DARK_GREY_COLOR = new Color(null, 60,179,113);
+    public static final Color DARK_GREY_COLOR = new Color(null,128,128,128);
     public static final Color BACKGROUND_RED = new Color(null,250,128,114);
     public static final Color BACKGROUND_BLUE = new Color(null,102,153,255);
     
