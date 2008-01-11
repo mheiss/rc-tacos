9c6da114c5dacc524b45176362ff12079947b687
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-01-11 19:39:31 +0000

    init

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/controller/DialysisOpenNewFormAction.java
@@ -0,0 +1,39 @@
+package at.rc.tacos.client.controller;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+
+import at.rc.tacos.client.view.DialysisForm;
+import at.rc.tacos.client.view.TransportForm;
+import at.rc.tacos.model.DialysisPatient;
+import at.rc.tacos.model.Transport;
+
+/**
+ * Opens the form to create a new dialysis entry
+ * @author b.thek
+ */
+public class DialysisOpenNewFormAction extends Action
+{
+	//properties
+	private TableViewer viewer;
+	
+	/**
+	 * Default class construtor.
+	 * @param viewer the table viewer
+	 */
+	public DialysisOpenNewFormAction(TableViewer viewer, String editingType)
+	{
+		this.viewer = viewer;
+		setText("Neuen Dialyseeintrag vornehmen");
+		setToolTipText("Öffnet ein Fenster, um einen neuen Dialyseeintrag zu erstellen.");
+	}
+	
+	@Override
+	public void run()
+	{
+		DialysisForm form = new DialysisForm();
+		form.open();
+	}
+}
