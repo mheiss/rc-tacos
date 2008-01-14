6a588103c2097435a79f13ff74f09fe98645b15c
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-01-14 23:29:50 +0000

    resetAllStati deleted

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/controller/CopyTransportAction.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/controller/CopyTransportAction.java
@@ -68,7 +68,7 @@ public class CopyTransportAction extends Action
     	t2.setReceiveTime(now);
     	t2.setToStreet(t1.getToStreet());
     	t2.setToCity(t1.getToCity());
-    	t2.resetAllStati();
+//    	t2.resetAllStati();
     	
     	NetWrapper.getDefault().sendAddMessage(Transport.ID, t2);
 	}
