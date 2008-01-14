0810de0426ed98253a8592f9f5dfc7869ab4736b
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-01-14 23:13:16 +0000

    doc extended

--- trunk/at.rc.tacos.model/src/at/rc/tacos/common/ITransportStatus.java
+++ trunk/at.rc.tacos.model/src/at/rc/tacos/common/ITransportStatus.java
@@ -10,25 +10,25 @@ package at.rc.tacos.common;
  */
 public interface ITransportStatus 
 {
-	/** Auftrag erteilt */
+	/** S0 Auftrag erteilt */
 	public final static int TRANSPORT_STATUS_ORDER_PLACED = 0;
-	/** Fahrzeug unterwegs */
+	/** S1 Fahrzeug unterwegs */
 	public final static int TRANSPORT_STATUS_ON_THE_WAY = 1;
-	/** Fahrzeug bei Patient */
+	/** S2 Fahrzeug bei Patient */
 	public final static int TRANSPORT_STATUS_AT_PATIENT = 2;
-	/** Abfahrt mit Patient */
+	/** S3 Abfahrt mit Patient */
 	public final static int TRANSPORT_STATUS_START_WITH_PATIENT = 3;
-	/** Ankunft Ziel */
+	/** S4 Ankunft Ziel */
 	public final static int TRANSPORT_STATUS_AT_DESTINATION = 4;
-	/** Ziel frei */
+	/** S5 Ziel frei */
 	public final static int TRANSPORT_STATUS_DESTINATION_FREE = 5;
-	/** eingerückt */
+	/** S6 eingerückt */
 	public final static int TRANSPORT_STATUS_CAR_IN_STATION = 6;
-	/** verlässt Einsatzgebiet */
+	/** S7 verlässt Einsatzgebiet */
 	public final static int TRANSPORT_STATUS_OUT_OF_OPERATION_AREA = 7;
-	/** Wieder im Einsatzgebiet */
+	/** S8 Wieder im Einsatzgebiet */
 	public final static int TRANSPORT_STATUS_BACK_IN_OPERATION_AREA = 8;
-	/** Sonderstatus, Sonstiges (z.B. Ambulanz, Essen,...) */
+	/** S9 Sonderstatus, Sonstiges (z.B. Ambulanz, Essen,...) */
 	public final static int TRANSPORT_STATUS_OTHER = 9;
 }
 
