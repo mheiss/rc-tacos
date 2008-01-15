840d581cb2cad4f722ad5809af19826702e694d9
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-01-15 23:48:30 +0000

    comment for the transport number

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/controller/DetachCarAction.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/controller/DetachCarAction.java
@@ -18,6 +18,7 @@ public class DetachCarAction extends Action
 {
 	//properties
 	private TableViewer viewer;
+	private String transportNumberOld;
 	
 	/**
 	 * Default class constructor.
@@ -37,13 +38,21 @@ public class DetachCarAction extends Action
 		ISelection selection = viewer.getSelection();
 		//get the selected transport
 		Transport transport = (Transport)((IStructuredSelection)selection).getFirstElement();
+		
+		
+		
 		//confirm the cancel
 		boolean cancelConfirmed = MessageDialog.openQuestion(
 				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), 
 				"Fahrzeug abziehen", "Möchten Sie das Fahrzeug wirklich vom Transport (" +transport.getFromStreet()+") abziehen?");
 		if (!cancelConfirmed) 
 			return;
+		
+		//---->
+		transportNumberOld = transport.getTransportNumber(); //TODO - Transportnummernverwaltung?
+		
 		transport.clearVehicleDetail();//TODO ok = ?
+//		transport.getStatusMessages().set(arg0, arg1); //TODO - clear all status messages!!!
 		NetWrapper.getDefault().sendUpdateMessage(Transport.ID, transport);
 	}
 }
