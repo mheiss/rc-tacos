800ddbcefcd034c1ff36c2d781df1e628874dadd
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-01-15 19:49:32 +0000

    init

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/controller/DetachCarAction.java
@@ -0,0 +1,49 @@
+package at.rc.tacos.client.controller;
+
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TableViewer;
+import org.eclipse.ui.PlatformUI;
+
+import at.rc.tacos.core.net.NetWrapper;
+import at.rc.tacos.model.Transport;
+
+/**
+ * Detaches the car from the transport
+ * @author b.thek
+ */
+public class DetachCarAction extends Action
+{
+	//properties
+	private TableViewer viewer;
+	
+	/**
+	 * Default class constructor.
+	 * @param viewer the table viewer
+	 */
+	public DetachCarAction(TableViewer viewer)
+	{
+		this.viewer = viewer;
+		setText("Fahrzeug abziehen");
+		setToolTipText("Zieht das Fahrzeug vom Transport ab");
+	}
+	
+	@Override
+	public void run()
+	{
+		//the selection
+		ISelection selection = viewer.getSelection();
+		//get the selected transport
+		Transport transport = (Transport)((IStructuredSelection)selection).getFirstElement();
+		//confirm the cancel
+		boolean cancelConfirmed = MessageDialog.openQuestion(
+				PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), 
+				"Fahrzeug abziehen", "Möchten Sie das Fahrzeug wirklich vom Transport (" +transport.getFromStreet()+") abziehen?");
+		if (!cancelConfirmed) 
+			return;
+		transport.clearVehicleDetail();//TODO ok = ?
+		NetWrapper.getDefault().sendUpdateMessage(Transport.ID, transport);
+	}
+}
