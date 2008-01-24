143f8fa4aafc9a102315a9f1625cb58b2bdd5535
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-01-24 22:49:36 +0000

    init

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/listeners/JobListener.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/listeners/JobListener.java
@@ -1,6 +1,52 @@
 package at.rc.tacos.client.listeners;
 
+import java.util.ArrayList;
+
+import at.rc.tacos.client.modelManager.JobManager;
+import at.rc.tacos.client.modelManager.ModelFactory;
+import at.rc.tacos.common.AbstractMessage;
+import at.rc.tacos.model.Job;
+
 public class JobListener extends ClientListenerAdapter
 {
-
+	//the job manager
+	JobManager manager = ModelFactory.getInstance().getJobList();
+	
+	@Override
+	public void add(AbstractMessage addMessage)
+	{
+		//cast to a job and add it
+        Job job = (Job)addMessage;
+        manager.add(job);
+	}
+	
+    @Override
+    public void remove(AbstractMessage removeMessage)
+    {
+    	//cast to a job and remove it
+    	Job job = (Job)removeMessage;
+        manager.remove(job);
+    }
+	
+	@Override
+	public void update(AbstractMessage updateMessage)
+	{
+		//cast to a job and add it
+		Job job = (Job)updateMessage;
+        manager.update(job);
+	}
+	
+    @Override
+    public void list(ArrayList<AbstractMessage> listMessage)
+    {
+    	//remove all stored job
+    	manager.removeAllEntries();
+        //loop and add all job
+        for(AbstractMessage detailObject:listMessage)
+        {
+        	//cast to a job and add it
+            Job job = (Job)detailObject;
+            manager.add(job);
+        }
+    }
 }
