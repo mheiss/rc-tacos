c31ba74f6b01411dbccd8d228c8051709fa09d4f
heissm <michael.heiss@outlook.at>
2008-01-24 20:11:23 +0000

    Fixed several problems on the server and the client side.

--- trunk/at.rc.tacos.core.net/src/at/rc/tacos/core/net/NetWrapper.java
+++ trunk/at.rc.tacos.core.net/src/at/rc/tacos/core/net/NetWrapper.java
@@ -296,7 +296,6 @@ public class NetWrapper extends Plugin implements INetListener
 		final String contentType = xmlFactory.getContentType();
 		final String queryString = xmlFactory.getQueryString();
 		final String userId = xmlFactory.getUserId();
-
 		System.out.println("Received: "+ userId+","+contentType+","+queryString);
 
 		//try to get a listener for this message
