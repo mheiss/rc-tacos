d76140e155b901a3941b8f588a5e9ea7c9d18005
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-01-25 15:23:44 +0000

    init

---
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/providers/CompetenceContentProvider.java
@@ -0,0 +1,21 @@
+package at.rc.tacos.client.providers;
+
+import org.eclipse.jface.viewers.IStructuredContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+import at.rc.tacos.client.modelManager.ModelFactory;
+
+public class CompetenceContentProvider implements IStructuredContentProvider
+{
+    @Override
+    public Object[] getElements(Object arg0)
+    {
+        return ModelFactory.getInstance().getCompetenceList().toArray();
+    }
+
+    @Override
+    public void dispose() { }
+
+    @Override
+    public void inputChanged(Viewer arg0, Object arg1, Object arg2) { }
+}
