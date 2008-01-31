cd3913148159920d7bd3ded346d4ec546c7cf6b3
heissm <michael.heiss@outlook.at>
2008-01-31 16:46:53 +0000

    deleted duplicate files.

--- trunk/at.rc.tacos.core.db/at.rc.tacos.core.db/src/at/rc/tacos/core/db/dao/DiseaseDAO.java
+++
@@ -1,9 +0,0 @@
-package at.rc.tacos.core.db.dao;
-
-import java.sql.SQLException;
-
-public interface DiseaseDAO
-{
-	Integer addDisease(String disease) throws SQLException;
-	boolean updateDisease(String columnName, String newValue, String diseaseID) throws SQLException;
-}
