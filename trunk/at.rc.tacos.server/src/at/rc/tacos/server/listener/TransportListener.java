2ec04a877edc2fb3e6eed3274be5f293ef5f34f4
heissm <michael.heiss@outlook.at>
2008-02-19 12:51:33 +0000

    transport management updated

--- trunk/at.rc.tacos.server/src/at/rc/tacos/server/listener/TransportListener.java
+++ trunk/at.rc.tacos.server/src/at/rc/tacos/server/listener/TransportListener.java
@@ -43,17 +43,14 @@ public class TransportListener extends ServerListenerAdapter
 	public ArrayList<AbstractMessage> handleListingRequest(QueryFilter queryFilter)  throws DAOException,SQLException
 	{
 		ArrayList<AbstractMessage> list = new ArrayList<AbstractMessage>();
-		List<Transport> transportList = null;
 		//if there is no filter -> request all
 		if(queryFilter == null || queryFilter.getFilterList().isEmpty())
 		{
 			System.out.println("WARNING: Listing of all transport entries is denied.");
 			throw new DAOException("TransportListener","Listing of all transport entries is denied");
 		}
-		else if(queryFilter.containsFilterType(IFilterTypes.TYPE_FILTER))
+		else if(queryFilter.containsFilterType(IFilterTypes.DATE_FILTER))
 		{
-			//get the query filter and parse it
-			final String type = queryFilter.getFilterValue(IFilterTypes.TYPE_FILTER);
 			//get the query filter and parse it to a date time
 			final String dateFilter = queryFilter.getFilterValue(IFilterTypes.DATE_FILTER);
 			long dateStart = MyUtils.stringToTimestamp(dateFilter,MyUtils.dateFormat);
@@ -62,11 +59,9 @@ public class TransportListener extends ServerListenerAdapter
 			calEnd.add(Calendar.DAY_OF_MONTH, 1);
 			long dateEnd = calEnd.getTimeInMillis();
 			//show current transports that are in progress
-			if(Transport.TRANSPORT_PROGRESS.equalsIgnoreCase(type))
-				transportList = transportDao.listTransports(dateStart, dateEnd);
+			List<Transport> transportList = transportDao.listTransports(dateStart, dateEnd);
 			//show the transports in the journal
-			else if(Transport.TRANSPORT_JOURNAL.equalsIgnoreCase(type))
-				transportList = transportDao.listArchivedTransports(dateStart, dateEnd);
+			List<Transport> journalList = transportDao.listArchivedTransports(dateStart, dateEnd);
 			//check
 			if(transportList == null)
 			{
@@ -74,6 +69,13 @@ public class TransportListener extends ServerListenerAdapter
 				throw new DAOException("TransportListener","Failed to list the transports by date from "+time);
 			}
 			list.addAll(transportList);
+			//check
+			if(journalList == null)
+			{
+				String time = MyUtils.timestampToString(dateStart, MyUtils.dateFormat) +" bis " +MyUtils.timestampToString(dateEnd, MyUtils.dateFormat);
+				throw new DAOException("TransportListener","Failed to list the archived transports by date from "+time);
+			}
+			list.addAll(journalList);
 		}
 		return list;
 	}
@@ -95,6 +97,11 @@ public class TransportListener extends ServerListenerAdapter
 	public AbstractMessage handleUpdateRequest(AbstractMessage updateObject)  throws DAOException,SQLException
 	{
 		Transport transport = (Transport)updateObject;
+		//
+		System.out.println("Update request for the transport");
+		System.out.println("car: "+transport.getVehicleDetail());
+		System.out.println("id: "+transport.getTransportId());
+		System.out.println("nummer: "+transport.getTransportNumber());
 		//generate a transport id if we do not have one
 		if(transport.getVehicleDetail() != null && transport.getTransportNumber() == 0)
 		{
@@ -106,7 +113,7 @@ public class TransportListener extends ServerListenerAdapter
 				throw new DAOException("TransportListener","Failed to generate a valid transport number for transport "+transport);
 			transport.setTransportNumber(transportNr);
 		}
-		
+
 		//STORNO OR FORWARD
 		if(transport.getTransportNumber() == Transport.TRANSPORT_CANCLED 
 				|| transport.getTransportNumber() == Transport.TRANSPORT_FORWARD)
@@ -115,7 +122,7 @@ public class TransportListener extends ServerListenerAdapter
 			if(!transportDao.cancelTransport(transport))
 				throw new DAOException("TransportListner","Failed to cancle the transport "+transport);
 		}
-		
+
 		//Vehicle is removed but we have a transport number -> cancle
 		if(transport.getVehicleDetail() == null && transport.getTransportNumber() > 0)
 		{
@@ -123,7 +130,7 @@ public class TransportListener extends ServerListenerAdapter
 			if(!transportDao.removeVehicleFromTransport(transport))
 				throw new DAOException("TransportListener","Failed to remove the transport from the vehicle");
 		}
-		
+
 		//send a simple update request to the dao
 		if(!transportDao.updateTransport(transport))
 			throw new DAOException("TransportListener","Failed to update the transport: "+transport);
