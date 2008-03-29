65afea0564cbed871754e8217a9dcf4874443109
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-03-29 21:18:38 +0000

    form color changed from red to grey

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/util/CustomColors.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/util/CustomColors.java
@@ -26,6 +26,7 @@ public final class CustomColors
     public static final Color DARK_GREY_COLOR = new Color(null,128,128,128);
     public static final Color BACKGROUND_RED = new Color(null,250,128,114);
     public static final Color BACKGROUND_BLUE = new Color(null,102,153,255);
+    public static final Color GREY_COLOR2 = new Color(null, 211,211,211);
     /** The color for the vehicle composites */
     public static final Color COLOR_BLUE = Util.getColor(209, 229, 249);
     public static final Color COLOR_GRAY =  Util.getColor(228, 236, 238);
@@ -40,14 +41,14 @@ public final class CustomColors
         if (formColors == null) 
         {
             formColors = new FormColors(display);
-            formColors.createColor( IFormColors.H_GRADIENT_START, RED_COLOR.getRGB());
-            formColors.createColor( IFormColors.H_GRADIENT_END, RED_COLOR.getRGB());
+            formColors.createColor( IFormColors.H_GRADIENT_START, GREY_COLOR2.getRGB());
+            formColors.createColor( IFormColors.H_GRADIENT_END, GREY_COLOR2.getRGB());
             formColors.createColor( IFormColors.H_BOTTOM_KEYLINE1, GREY_COLOR.getRGB());
-            formColors.createColor( IFormColors.H_BOTTOM_KEYLINE2, RED_COLOR.getRGB());
+            formColors.createColor( IFormColors.H_BOTTOM_KEYLINE2, GREY_COLOR2.getRGB());
             formColors.createColor( IFormColors.TITLE, HEADING_COLOR.getRGB());
             formColors.createColor( IFormColors.TB_BG, SECTION_BACKGROUND.getRGB());
             formColors.createColor( IFormColors.TB_BORDER, SECTION_BACKGROUND.getRGB());
-            formColors.createColor( IFormColors.SEPARATOR, RED_COLOR.getRGB()); 
+            formColors.createColor( IFormColors.SEPARATOR,GREY_COLOR2.getRGB()); 
         }
         return formColors;
     }
