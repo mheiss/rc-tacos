1bfd4d3782f649ab39f27cd0ce09e67bef334cde
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-03-29 20:07:31 +0000

    saveHyperlink renamed, addHyperlink removed

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/editors/JobEditor.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/editors/JobEditor.java
@@ -45,7 +45,7 @@ public class JobEditor extends EditorPart implements PropertyChangeListener
 	private FormToolkit toolkit;
 	private ScrolledForm form;
 	
-	private ImageHyperlink saveHyperlink,addHyperlink,removeHyperlink;
+	private ImageHyperlink saveHyperlink,removeHyperlink;
 	private Text id,name;
 
 	//managed data
@@ -113,7 +113,7 @@ public class JobEditor extends EditorPart implements PropertyChangeListener
 
 		//Create the hyperlink to save the changes
 		saveHyperlink = toolkit.createImageHyperlink(client, SWT.NONE);
-		saveHyperlink.setText("Neue Verwendung anlegen");
+		saveHyperlink.setText("Neue Verwendung speichern");
 		saveHyperlink.setImage(ImageFactory.getInstance().getRegisteredImage("admin.save"));
 		saveHyperlink.addHyperlinkListener(new HyperlinkAdapter() 
 		{
@@ -125,19 +125,6 @@ public class JobEditor extends EditorPart implements PropertyChangeListener
 			}
 		});
 		
-		//create the hyperlink to add a new job
-		addHyperlink = toolkit.createImageHyperlink(client, SWT.NONE);
-		addHyperlink.setText("Verwendung anlegen");
-		addHyperlink.setImage(ImageFactory.getInstance().getRegisteredImage("admin.jobAdd"));
-		addHyperlink.addHyperlinkListener(new HyperlinkAdapter()
-		{
-			@Override
-			public void linkActivated(HyperlinkEvent e) 
-			{
-				EditorNewJobAction newAction = new EditorNewJobAction(PlatformUI.getWorkbench().getActiveWorkbenchWindow());
-				newAction.run();
-			}
-		});
 		//Create the hyperlink to remove the competence
 		removeHyperlink = toolkit.createImageHyperlink(client, SWT.NONE);
 		removeHyperlink.setText("Verwendung löschen");
@@ -210,7 +197,6 @@ public class JobEditor extends EditorPart implements PropertyChangeListener
 		{
 			//adjust the links
 			saveHyperlink.setText("Änderungen speichern");
-			addHyperlink.setVisible(true);
 		}
 		id.setText(String.valueOf(job.getId()));
 		name.setText(job.getJobName());
