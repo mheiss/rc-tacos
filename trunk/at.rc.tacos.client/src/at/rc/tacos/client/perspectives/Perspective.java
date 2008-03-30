ac4450485f560a8211297a67540080ef0de910df
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-03-30 22:51:56 +0000

    new vehicle table view added

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/perspectives/Perspective.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/perspectives/Perspective.java
@@ -23,8 +23,9 @@ public class Perspective implements IPerspectiveFactory
         layout.setFixed(false);
         //the main components
         layout.addStandaloneView(NavigationView.ID, false, IPageLayout.TOP, 0.10f, editorArea);
-        layout.addStandaloneView(VehiclesView.ID,false, IPageLayout.LEFT, 0.45f, editorArea);
-        layout.addStandaloneView(PersonalView.ID,false, IPageLayout.RIGHT, 0.45f, editorArea);
+        layout.addStandaloneView(VehiclesViewTable.ID,false, IPageLayout.LEFT, 0.07f, editorArea);
+        layout.addStandaloneView(VehiclesView.ID,false, IPageLayout.LEFT, 0.50f, editorArea);
+        layout.addStandaloneView(PersonalView.ID,false, IPageLayout.LEFT, 0.25f, editorArea);
         layout.addStandaloneView(InfoView.ID, false, IPageLayout.TOP, 0.35f, PersonalView.ID);        
 	}
 }
