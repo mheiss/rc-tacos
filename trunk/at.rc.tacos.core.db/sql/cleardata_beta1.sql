6dcb1c26fc37a157fada0cddb2cdc29405fa9b25
stoffl666 <stoffl666@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-04-04 20:46:18 +0000

    cleardata, droptables geht insertdata, createtable,  geht zwar aber bestimmte sachen noch nicht optimal

---
+++ trunk/at.rc.tacos.core.db/sql/cleardata_beta1.sql
@@ -0,0 +1,43 @@
+--  Clear data script version 1.5, lastChanged: 28.01.2008
+DELETE FROM transport_selected;
+
+DELETE FROM assigned_vehicle;
+
+DELETE FROM transportstate;
+
+DELETE FROM vehicles;
+
+DELETE FROM roster;
+
+DELETE FROM transports;
+
+DELETE FROM staffmember_competence;
+
+DELETE FROM phone_staffmember;
+
+DELETE FROM staffmembers;
+
+DELETE FROM location;
+
+DELETE FROM dayinfo;
+
+DELETE FROM dialysis;
+
+DELETE FROM dialysis_transport;
+
+DELETE FROM competences;
+
+DELETE FROM tmptransports;
+
+DELETE FROM caller;
+
+DELETE FROM disease;
+
+DELETE FROM userlogin;
+
+DELETE FROM phonenumbers;
+
+DELETE FROM job;
+
+DELETE FROM servicetype;
+
