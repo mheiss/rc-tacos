8f4129acb507ca56905511964a5482252fec50d6
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-04-09 21:02:17 +0000

    bug fixed! :-(

--- trunk/at.rc.tacos.core.db/src/at/rc/tacos/core/db/dao/mysql/CallerDAOMySQL.java
+++ trunk/at.rc.tacos.core.db/src/at/rc/tacos/core/db/dao/mysql/CallerDAOMySQL.java
@@ -5,7 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import at.rc.tacos.core.db.DataSource;
-import at.rc.tacos.core.db.SQLQueries;
+import at.rc.tacos.core.db.Queries;
 import at.rc.tacos.core.db.dao.CallerDAO;
 import at.rc.tacos.model.CallerDetail;
 
@@ -13,7 +13,7 @@ public class CallerDAOMySQL implements CallerDAO
 {
 	//The data source to get the connection and the queries file
 	private final DataSource source = DataSource.getInstance();
-	private final SQLQueries queries = SQLQueries.getInstance();
+	private final Queries queries = Queries.getInstance();
 
 	@Override
 	public int addCaller(CallerDetail notifierDetail) throws SQLException
