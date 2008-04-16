ce77276576e2180fec310570e83c8648e31c06f5
heissm <michael.heiss@outlook.at>
2008-04-16 13:43:31 +0000

    Refactored the network code.

--- trunk/at.rc.tacos.server/src/at/rc/tacos/server/controller/ClientHandler.java
+++ trunk/at.rc.tacos.server/src/at/rc/tacos/server/controller/ClientHandler.java
@@ -1,9 +1,7 @@
 package at.rc.tacos.server.controller;
 
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.List;
 
 import org.apache.log4j.Logger;
@@ -17,7 +15,6 @@ import at.rc.tacos.model.Login;
 import at.rc.tacos.model.Logout;
 import at.rc.tacos.model.QueryFilter;
 import at.rc.tacos.model.SystemMessage;
-import at.rc.tacos.util.MyUtils;
 import at.rc.tacos.common.*;
 
 /**
@@ -26,9 +23,9 @@ import at.rc.tacos.common.*;
  */
 public class ClientHandler implements INetListener
 {
-    //the logger
-    private static Logger logger = Logger.getLogger(ClientHandler.class);
-	
+	//the logger
+	private static Logger logger = Logger.getLogger(ClientHandler.class);
+
 	@Override
 	public synchronized void dataReceived(NetEvent ne)
 	{
@@ -39,6 +36,7 @@ public class ClientHandler implements INetListener
 
 		//get the type of the item
 		final String userId = xmlFactory.getUserId();
+		final String sequenceId = xmlFactory.getSequenceId();
 		final String contentType = xmlFactory.getContentType();
 		final String queryString = xmlFactory.getQueryString();
 		final QueryFilter queryFilter = xmlFactory.getQueryFilter();
@@ -52,13 +50,31 @@ public class ClientHandler implements INetListener
 		final ClientSession session = server.getSession(ne.getClient());
 
 		logger.debug("QUERY from "+userId+": "+contentType+"->"+queryString);
+		
+		//send back the keep alive package
+		if(IModelActions.KEEP_ALIVE.equalsIgnoreCase(queryString))
+		{
+			AbstractMessageInfo info = new AbstractMessageInfo();
+			info.setSequenceId(sequenceId);
+			info.setContentType(SystemMessage.ID);
+			info.setQueryString(IModelActions.KEEP_ALIVE);
+			server.sendMessage(session, info);
+			return;
+		}
 
 		//the client is not authenticated, no login request -> not accepted
 		if(!session.isAuthenticated() &! Login.ID.equalsIgnoreCase(contentType))
 		{
 			logger.warn("Client not authenticated, login first");
 			SystemMessage system = new SystemMessage("Client not authenticated, login first",SystemMessage.TYPE_INFO);
-			server.sendMessage(session,SystemMessage.ID,IModelActions.SYSTEM,system);
+			//setup the message info container
+			AbstractMessageInfo info = new AbstractMessageInfo();
+			info.setSequenceId(sequenceId);
+			info.setContentType(SystemMessage.ID);
+			info.setQueryString(IModelActions.SYSTEM);
+			info.setMessage(system);
+			//send back to the client
+			server.sendMessage(session,info);
 			return;
 		}
 
@@ -67,12 +83,25 @@ public class ClientHandler implements INetListener
 		{
 			logger.error("No listener found for the message type: "+contentType);
 			//notify the sender
-			SystemMessage sysMess = new SystemMessage("No listener found for the message type: "+contentType,SystemMessage.TYPE_INFO);
-			server.sendMessage(session,SystemMessage.ID,IModelActions.SYSTEM,sysMess);
+			SystemMessage system = new SystemMessage("No listener found for the message type: "+contentType,SystemMessage.TYPE_INFO);
+			//setup the message info container
+			AbstractMessageInfo info = new AbstractMessageInfo();
+			info.setSequenceId(sequenceId);
+			info.setContentType(SystemMessage.ID);
+			info.setQueryString(IModelActions.SYSTEM);
+			info.setMessage(system);
+			//send back to the client
+			server.sendMessage(session,info);
 			return;
 		}
 		try
 		{
+			//setup the message info container
+			AbstractMessageInfo info = new AbstractMessageInfo();
+			info.setSequenceId(sequenceId);
+			info.setContentType(contentType);
+			info.setQueryString(queryString);
+			
 			//login request
 			if(IModelActions.LOGIN.equalsIgnoreCase(queryString))
 			{
@@ -81,88 +110,93 @@ public class ClientHandler implements INetListener
 				//add the client to the authenticated list
 				if(loginResult.isLoggedIn())
 					session.setAuthenticated(loginResult.getUsername(),loginResult.isWebClient());
-				//notify the sender about the result
-				server.sendMessage(session,contentType,queryString,loginResult);
+				//send back
+				info.setMessage(loginResult);
+				server.sendMessage(session,info);
+				return;
 			}
 			//logout request
-			else if(IModelActions.LOGOUT.equalsIgnoreCase(queryString))
+			if(IModelActions.LOGOUT.equalsIgnoreCase(queryString))
 			{
 				Logout logoutResult = (Logout)listener.handleLogoutRequest(objects.get(0));
 				//remove the client form the list of authenticated clients if successfully
 				if(logoutResult.isLoggedOut())
 					session.setDeAuthenticated();
 				server.clientDisconnected(session.getConnection());
-				server.sendMessage(session, contentType, queryString, logoutResult);
+				//send back to the client
+				info.setMessage(logoutResult);
+				server.sendMessage(session,info);
+				return;
 			}
 			//add request
-			else if(IModelActions.ADD.equalsIgnoreCase(queryString))
+			if(IModelActions.ADD.equalsIgnoreCase(queryString))
 			{
 				AbstractMessage resultAddMessage = listener.handleAddRequest(objects.get(0));
-				//send the added item to all connected clients
-				server.brodcastMessage(userId, contentType, queryString, resultAddMessage);
-				//if we have a web client -> send the request back
-				if(session.isWebClient())
-					server.sendMessage(session, contentType, queryString, resultAddMessage); 
+				info.setMessage(resultAddMessage);
+				server.brodcastMessage(session,info);
+				return;
 			}
 			//add all request
-			else if(IModelActions.ADD_ALL.equalsIgnoreCase(queryString))
+			if(IModelActions.ADD_ALL.equalsIgnoreCase(queryString))
 			{
 				List<AbstractMessage> resultAddList = listener.handleAddAllRequest(objects);
-				//send the added item to all connected clients
-				server.brodcastMessage(userId, contentType, queryString, resultAddList);
-				//if we have a web client -> send the request back
-				if(session.isWebClient())
-					server.sendMessage(session, contentType, queryString, resultAddList); 
+				info.setMessageList(resultAddList);
+				server.brodcastMessage(session,info);
+				return;
 			}
 			//remove request
-			else if(IModelActions.REMOVE.equalsIgnoreCase(queryString))
+			if(IModelActions.REMOVE.equalsIgnoreCase(queryString))
 			{
 				AbstractMessage resultRemoveMessage = listener.handleRemoveRequest(objects.get(0));
-				//send the removed item
-				server.brodcastMessage(userId, contentType, queryString, resultRemoveMessage);
-				//if we have a web client -> send the request back
-				if(session.isWebClient())
-					server.sendMessage(session, contentType, queryString, resultRemoveMessage);
+				info.setMessage(resultRemoveMessage);
+				server.brodcastMessage(session,info);
+				return;
 			}
 			//update request
-			else if(IModelActions.UPDATE.equalsIgnoreCase(queryString))
+			if(IModelActions.UPDATE.equalsIgnoreCase(queryString))
 			{
 				AbstractMessage resultUpdateMessage = listener.handleUpdateRequest(objects.get(0));
-				//send the updated item
-				server.brodcastMessage(userId, contentType, queryString, resultUpdateMessage);
-				//if we have a web client -> send the request back
-				if(session.isWebClient())
-					server.sendMessage(session, contentType, queryString, resultUpdateMessage);
+				info.setMessage(resultUpdateMessage);
+				server.brodcastMessage(session,info);
+				return;
 			}
-			else if(IModelActions.LIST.equalsIgnoreCase(queryString))
+			//listing request
+			if(IModelActions.LIST.equalsIgnoreCase(queryString))
 			{
-				long startQuery = Calendar.getInstance().getTimeInMillis();
 				List<AbstractMessage> resultMessageList = listener.handleListingRequest(queryFilter);
-				long endQuery = Calendar.getInstance().getTimeInMillis();
-				logger.info("OPERATION TIME for "+contentType +" listing: "+MyUtils.timestampToString(endQuery-startQuery, new SimpleDateFormat("mm:ss"))+ "("+(endQuery-startQuery)+"ms)");
-				//send the listing
-				server.sendMessage(session, contentType, queryString, resultMessageList);
+				info.setMessageList(resultMessageList);
+				//send back to the client
+				server.sendMessage(session, info);
+				return;
 			}
-			else
-			{   
-				logger.error("No handler found for queryString: "+queryString);
-				SystemMessage sysMes = new SystemMessage("No handler found for queryString: "+queryString,SystemMessage.TYPE_ERROR);
-				server.sendMessage(session,SystemMessage.ID,IModelActions.SYSTEM,sysMes);
-			}  
 		}
 		//catch all sql errors that occured during the operations with the listener classes
 		catch(SQLException sqle)
 		{
 			logger.error("SQL-Error: "+sqle.getMessage(),sqle);
 			SystemMessage system = new SystemMessage("SQL-Error:"+sqle.getMessage(),SystemMessage.TYPE_ERROR);
-			server.sendMessage(session, SystemMessage.ID, IModelActions.SYSTEM, system);
+			//setup the message info container
+			AbstractMessageInfo info = new AbstractMessageInfo();
+			info.setSequenceId(sequenceId);
+			info.setContentType(SystemMessage.ID);
+			info.setQueryString(IModelActions.SYSTEM);
+			info.setMessage(system);
+			//send back to the client
+			server.sendMessage(session,info);
 		}
 		//catch all error during the operations with the dao listener classes
 		catch(DAOException daoe)
 		{
 			logger.error("DAO-Exception: "+daoe.getMessage(),daoe);
 			SystemMessage system = new SystemMessage(daoe.getMessage(),SystemMessage.TYPE_ERROR);
-			server.sendMessage(session, SystemMessage.ID, IModelActions.SYSTEM, system);  
+			//setup the message info container
+			AbstractMessageInfo info = new AbstractMessageInfo();
+			info.setSequenceId(sequenceId);
+			info.setContentType(SystemMessage.ID);
+			info.setQueryString(IModelActions.SYSTEM);
+			info.setMessage(system);
+			//send back to the client
+			server.sendMessage(session,info);  
 		}
 	}
 
