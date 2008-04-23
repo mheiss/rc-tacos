1c48ff586d2704c981da4cd8f0b3186afd159e84
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-04-23 22:52:24 +0000

    menu changed

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/ApplicationActionBarAdvisor.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/ApplicationActionBarAdvisor.java
@@ -6,16 +6,11 @@ import org.eclipse.ui.*;
 import org.eclipse.ui.actions.*;
 import org.eclipse.ui.application.*;
 import org.eclipse.ui.actions.ActionFactory.*;
-
 import at.rc.tacos.client.controller.ConnectionWizardAction;
-import at.rc.tacos.client.modelManager.SessionManager;
 import at.rc.tacos.client.perspectives.SwitchToAdminPerspective;
 import at.rc.tacos.client.perspectives.SwitchToClientPerspective;
 import at.rc.tacos.client.perspectives.SwitchToLogPerspective;
-import at.rc.tacos.client.perspectives.SwitchToTransportDialysePerspective;
-import at.rc.tacos.client.perspectives.SwitchToTransportJournalPerspective;
-import at.rc.tacos.client.perspectives.SwitchToTransportPerspective;
-import at.rc.tacos.client.perspectives.SwitchToTransportPrebookingPerspective;
+
 
 /**
  * An action bar advisor is responsible for creating, adding, and disposing of the
@@ -29,13 +24,8 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 	private IWorkbenchAction aboutAction;
 	private ConnectionWizardAction conWizard;
 	private SwitchToClientPerspective switchToClient;
-	private SwitchToTransportPerspective switchToTransport;
-	private SwitchToTransportDialysePerspective switchToDialyse;
-	private SwitchToTransportPrebookingPerspective switchToPrebooking;
-	private SwitchToTransportJournalPerspective switchToJournal;
 	private SwitchToAdminPerspective switchToAdmin;
 	private SwitchToLogPerspective switchToLog;
-	private IContributionItem viewList;
 
 	/**
 	 * Default class constructor.
@@ -57,13 +47,8 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 		aboutAction = ActionFactory.ABOUT.create(window);
 		conWizard = new ConnectionWizardAction(window);
 		switchToClient = new SwitchToClientPerspective();
-		switchToTransport = new SwitchToTransportPerspective();
-		switchToPrebooking = new SwitchToTransportPrebookingPerspective();
-		switchToDialyse = new SwitchToTransportDialysePerspective();
-		switchToJournal = new SwitchToTransportJournalPerspective();
 		switchToAdmin = new SwitchToAdminPerspective();
 		switchToLog = new SwitchToLogPerspective();
-		viewList = ContributionItemFactory.VIEWS_SHORTLIST.create(window); 
 		register(aboutAction);
 		register(exitAction);
 		register(conWizard);
@@ -75,30 +60,20 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 	@Override
 	protected void fillMenuBar(IMenuManager menuBar) 
 	{
-		//get the authorization;
-		String authorization = SessionManager.getInstance().getLoginInformation().getAuthorization();
 		//the file menu
 		MenuManager fileMenu = new MenuManager("&File", IWorkbenchActionConstants.M_FILE);
 		fileMenu.add(exitAction);
 		
 		//the admin sub menu
 		MenuManager adminMenu = new MenuManager("&Administation");
-		if(authorization.equalsIgnoreCase("Administrator"))
-		{
-			adminMenu.add(switchToLog);
-			adminMenu.add(switchToAdmin);
-		}
-		adminMenu.add(viewList);
+		adminMenu.add(switchToLog);
+		adminMenu.add(switchToAdmin);
 		
 		//window menu
 		MenuManager windowMenu = new MenuManager("&Window");
 		windowMenu.add(adminMenu);
 		windowMenu.add(new Separator());
 		windowMenu.add(switchToClient);
-		windowMenu.add(switchToTransport);
-		windowMenu.add(switchToPrebooking);
-		windowMenu.add(switchToDialyse);
-		windowMenu.add(switchToJournal);
 
 		//help menu
 		MenuManager helpMenu = new MenuManager("&Help");
