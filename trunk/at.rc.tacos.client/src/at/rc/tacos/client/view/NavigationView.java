2abaa4a0b976c966e3232e57e7f91f50d3f19241
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-04-23 22:52:49 +0000

    some perspective-buttons removed

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/view/NavigationView.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/view/NavigationView.java
@@ -9,20 +9,11 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.ui.part.ViewPart;
-
 import at.rc.tacos.client.controller.OpenDialysisTransportAction;
 import at.rc.tacos.client.controller.OpenEmergencyTransportAction;
 import at.rc.tacos.client.controller.OpenTransportAction;
 import at.rc.tacos.client.controller.PersonalNewEntryAction;
 import at.rc.tacos.client.modelManager.MyToolbarManager;
-import at.rc.tacos.client.modelManager.SessionManager;
-import at.rc.tacos.client.perspectives.SwitchToAdminPerspective;
-import at.rc.tacos.client.perspectives.SwitchToClientPerspective;
-import at.rc.tacos.client.perspectives.SwitchToLogPerspective;
-import at.rc.tacos.client.perspectives.SwitchToTransportDialysePerspective;
-import at.rc.tacos.client.perspectives.SwitchToTransportJournalPerspective;
-import at.rc.tacos.client.perspectives.SwitchToTransportPerspective;
-import at.rc.tacos.client.perspectives.SwitchToTransportPrebookingPerspective;
 import at.rc.tacos.client.util.CustomColors;
 import at.rc.tacos.factory.ImageFactory;
 
@@ -45,24 +36,6 @@ public class NavigationView extends ViewPart
 	    parent.setBackground(CustomColors.SECTION_BACKGROUND);
 	    
 	    //create a group for the perspective switch
-		final Group perspectiveGroup = new Group(parent, SWT.NONE);
-		perspectiveGroup.setLayout(new GridLayout());
-		perspectiveGroup.setLayoutData(new GridData());
-		perspectiveGroup.setBackground(CustomColors.SECTION_BACKGROUND);
-		perspectiveGroup.setText("Ansichten");
-	    
-	    //the toolbar to switch the views
-	    final MyToolbarManager tbmView = new MyToolbarManager(new ToolBar(perspectiveGroup,SWT.FLAT));
-	    tbmView.getControl().setBackground(CustomColors.SECTION_BACKGROUND);
-	    tbmView.getControl().setLayoutData(new GridData(SWT.BEGINNING,SWT.BEGINNING,false,false));
-	    tbmView.add(new SwitchToTransportPrebookingPerspective());
-	    tbmView.add(new SwitchToTransportPerspective());
-	    tbmView.add(new SwitchToTransportJournalPerspective());
-	    tbmView.add(new SwitchToTransportDialysePerspective());
-	    tbmView.add(new SwitchToClientPerspective());
-	    tbmView.update(true);
-	    
-	    //create a group for the perspective switch
 		final Group createGroup = new Group(parent, SWT.NONE);
 		createGroup.setLayout(new GridLayout());
 		createGroup.setLayoutData(new GridData());
@@ -79,23 +52,6 @@ public class NavigationView extends ViewPart
 	    tbmCreate.add(new OpenTransportAction());
 	    tbmCreate.add(new OpenDialysisTransportAction());
 	    tbmCreate.update(true);
-	    
-	    //creat ea group for other
-		final Group otherGroup = new Group(parent, SWT.NONE);
-		otherGroup.setLayout(new GridLayout());
-		otherGroup.setLayoutData(new GridData());
-		otherGroup.setBackground(CustomColors.SECTION_BACKGROUND);
-		otherGroup.setText("Sonstiges");
-		
-		final MyToolbarManager othView = new MyToolbarManager(new ToolBar(otherGroup,SWT.FLAT));
-		othView.getControl().setBackground(CustomColors.SECTION_BACKGROUND);
-		othView.getControl().setLayoutData(new GridData(SWT.BEGINNING,SWT.BEGINNING,false,false));
-		othView.add(new SwitchToLogPerspective());
-		othView.add(new SwitchToAdminPerspective());
-		othView.update(true);
-		String authorization = SessionManager.getInstance().getLoginInformation().getAuthorization();
-		if(!authorization.equalsIgnoreCase("Administrator"))
-			otherGroup.setVisible(false);
 
 	    Composite comp1 = new Composite(parent,SWT.NONE);
 	    comp1.setBackground(CustomColors.SECTION_BACKGROUND);
