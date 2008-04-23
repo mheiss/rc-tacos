180667efcc922cd5c3587769df8be3680fe5ca3b
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-04-23 22:52:09 +0000

    folder added layout changed

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/perspectives/Perspective.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/perspectives/Perspective.java
@@ -1,5 +1,6 @@
 package at.rc.tacos.client.perspectives;
 
+import org.eclipse.ui.IFolderLayout;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 import at.rc.tacos.client.view.*;
@@ -22,14 +23,16 @@ public class Perspective implements IPerspectiveFactory
         layout.setEditorAreaVisible(false);
         layout.setFixed(false);
         //the main components
+        IFolderLayout folder = layout.createFolder(TransportFolder.ID, IPageLayout.TOP, 0.10f, editorArea);
+        folder.addView(UnderwayTransportsView.ID);
+        folder.addView(PrebookingView.ID);
+        folder.addView(JournalView.ID);
+        folder.addView(DialysisView.ID);
+       
         layout.addStandaloneView(NavigationView.ID, false, IPageLayout.TOP, 0.10f, editorArea);
         layout.addStandaloneView(VehiclesViewTable.ID,false, IPageLayout.LEFT, 0.07f, editorArea);
         layout.addStandaloneView(VehiclesView.ID,false, IPageLayout.LEFT, 0.50f, editorArea);
         layout.addStandaloneView(PersonalView.ID,false, IPageLayout.LEFT, 0.25f, editorArea);
         layout.addStandaloneView(InfoView.ID, false, IPageLayout.TOP, 0.35f, PersonalView.ID); 
-        layout.addStandaloneView(UnderwayTransportsView.ID, true, IPageLayout.RIGHT, 0.30f, editorArea);
-        layout.addStandaloneView(PrebookingView.ID, true, IPageLayout.RIGHT, 0.30f, editorArea);
-        layout.addStandaloneView(JournalView.ID, true, IPageLayout.RIGHT, 0.30f, editorArea);
-        layout.addStandaloneView(DialysisView.ID, true, IPageLayout.RIGHT, 0.30f, editorArea);
 	}
 }
