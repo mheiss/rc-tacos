ea081a437c4d063a08c169d152936fcf8c487133
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-04-26 01:55:11 +0000

    shortcuts added (accelerator)

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/ApplicationActionBarAdvisor.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/ApplicationActionBarAdvisor.java
@@ -8,6 +8,10 @@ import org.eclipse.ui.application.*;
 import org.eclipse.ui.actions.ActionFactory.*;
 
 import at.rc.tacos.client.controller.ConnectionWizardAction;
+import at.rc.tacos.client.controller.OpenDialysisTransportAction;
+import at.rc.tacos.client.controller.OpenEmergencyTransportAction;
+import at.rc.tacos.client.controller.OpenTransportAction;
+import at.rc.tacos.client.controller.PersonalNewEntryAction;
 import at.rc.tacos.client.perspectives.SwitchToAdminPerspective;
 import at.rc.tacos.client.perspectives.SwitchToClientPerspective;
 import at.rc.tacos.client.perspectives.SwitchToLogPerspective;
@@ -28,6 +32,10 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 	private SwitchToAdminPerspective switchToAdmin;
 	private SwitchToLogPerspective switchToLog;
 	private IContributionItem viewList;
+	private PersonalNewEntryAction personalNewEntryAction;
+	private OpenEmergencyTransportAction openEmergencyTransportAction;
+	private OpenTransportAction openTransportAction;
+	private OpenDialysisTransportAction openDialysisTransportAction;
 
 	/**
 	 * Default class constructor.
@@ -46,12 +54,22 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 	{
 		// Creates the actions and registers them.
 		exitAction = ActionFactory.QUIT.create(window);
+		exitAction.setAccelerator(SWT.CTRL + 'I');
 		aboutAction = ActionFactory.ABOUT.create(window);
 		conWizard = new ConnectionWizardAction(window);
 		switchToClient = new SwitchToClientPerspective();
+		switchToClient.setAccelerator(SWT.CTRL + 'R');
 		switchToAdmin = new SwitchToAdminPerspective();
 		switchToLog = new SwitchToLogPerspective();
 		viewList = ContributionItemFactory.VIEWS_SHORTLIST.create(window);
+		personalNewEntryAction = new PersonalNewEntryAction();
+		personalNewEntryAction.setAccelerator(SWT.CTRL + 'D');
+		openEmergencyTransportAction = new OpenEmergencyTransportAction();
+		openEmergencyTransportAction.setAccelerator(SWT.CTRL + 'O');
+		openTransportAction = new OpenTransportAction();
+		openTransportAction.setAccelerator(SWT.CTRL + 'T');
+		openDialysisTransportAction = new OpenDialysisTransportAction();
+		openDialysisTransportAction.setAccelerator(SWT.CTRL + 'L');
 		register(aboutAction);
 		register(exitAction);
 		register(conWizard);
@@ -64,8 +82,14 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 	protected void fillMenuBar(IMenuManager menuBar) 
 	{
 		//the file menu
-		MenuManager fileMenu = new MenuManager("&File", IWorkbenchActionConstants.M_FILE);
+		MenuManager fileMenu = new MenuManager("&Datei", IWorkbenchActionConstants.M_FILE);
 		fileMenu.add(exitAction);
+		
+		MenuManager newMenu = new MenuManager("&Anlegen");
+		newMenu.add(personalNewEntryAction);
+		newMenu.add(openEmergencyTransportAction);
+		newMenu.add(openTransportAction);
+		newMenu.add(openDialysisTransportAction);
 		
 		//the admin sub menu
 		MenuManager adminMenu = new MenuManager("&Administation");
@@ -73,14 +97,14 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 		adminMenu.add(switchToAdmin);
 		
 		//window menu
-		MenuManager windowMenu = new MenuManager("&Window");
+		MenuManager windowMenu = new MenuManager("&Fenster");
 		windowMenu.add(adminMenu);
 		windowMenu.add(new Separator());
 		windowMenu.add(switchToClient);
 		 adminMenu.add(viewList);
 
 		//help menu
-		MenuManager helpMenu = new MenuManager("&Help");
+		MenuManager helpMenu = new MenuManager("&Hilfe");
 		helpMenu.add(aboutAction);
 		helpMenu.add(conWizard);
 
@@ -88,6 +112,7 @@ public class ApplicationActionBarAdvisor extends ActionBarAdvisor
 		menuBar.add(fileMenu);
 		menuBar.add(windowMenu);
 		menuBar.add(helpMenu);
+		menuBar.add(newMenu);
 		
 	}
 
