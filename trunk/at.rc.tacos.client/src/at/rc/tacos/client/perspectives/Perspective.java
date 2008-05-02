8baae61ebc55541039c1b97500324bfebfb62379
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-05-02 11:46:22 +0000

    position of transport filter changed

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/perspectives/Perspective.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/perspectives/Perspective.java
@@ -22,23 +22,30 @@ public class Perspective implements IPerspectiveFactory
         String editorArea = layout.getEditorArea();
         layout.setEditorAreaVisible(false);
         layout.setFixed(false);
+        
         //the main components
         IFolderLayout folder = layout.createFolder(TransportFolder.ID, IPageLayout.TOP, 0.10f, editorArea);
         folder.addView(UnderwayTransportsView.ID);
-        folder.addView(PrebookingView.ID);
         folder.addView(JournalView.ID);
+        folder.addView(PrebookingView.ID);
+        
         folder.addView(DialysisView.ID);
        
         layout.addStandaloneView(NavigationView.ID, false, IPageLayout.TOP, 0.10f, editorArea);
-        layout.addStandaloneView(VehiclesViewTable.ID,false, IPageLayout.LEFT, 0.07f, editorArea);
-        layout.addStandaloneView(VehiclesView.ID,false, IPageLayout.LEFT, 0.50f, editorArea);
-        layout.addStandaloneView(PersonalView.ID,false, IPageLayout.LEFT, 0.25f, editorArea);
+        layout.addStandaloneView(FilterView.ID,true, IPageLayout.LEFT, 0.11f, editorArea);
+        layout.addStandaloneView(VehiclesViewTable.ID,false, IPageLayout.LEFT, 0.055f, editorArea);
+        layout.addStandaloneView(VehiclesView.ID,false, IPageLayout.LEFT, 0.512f, editorArea);
+        layout.addStandaloneView(PersonalView.ID,false, IPageLayout.LEFT, 0.243f, editorArea);
         layout.addStandaloneView(InfoView.ID, false, IPageLayout.TOP, 0.35f, PersonalView.ID); 
+       
         
         //add the shortcuts to open the views
         layout.addShowViewShortcut(UnderwayTransportsView.ID);
         layout.addShowViewShortcut(PrebookingView.ID);
         layout.addShowViewShortcut(DialysisView.ID);
         layout.addShowViewShortcut(JournalView.ID);
+        
+        
+//        layout.getViewLayout(JournalView.ID).setCloseable(false);//doesn't work //TODO
 	}
 }
