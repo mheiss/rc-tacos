f5c4e9f568c1a2321613ffe40b3ad5585868eb1e
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-05-07 21:54:49 +0000

    filter fields added

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/providers/TransportViewFilter.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/providers/TransportViewFilter.java
@@ -8,21 +8,29 @@ import at.rc.tacos.model.Transport;
 public class TransportViewFilter  extends ViewerFilter
 {
 	//the criteria to filter
+	private String trNr;
 	private String from;
 	private String patient;
 	private String to;
 	private String location;
+	private String priority;
+	private String vehicle;
+	private String disease;
 
 	/**
 	 * Default class constructor for the address filter.
 	 * @param value the street or the city to filter
 	 */
-	public TransportViewFilter(String from,String patient,String to, String location)
+	public TransportViewFilter(String trNr, String from,String patient,String to, String location, String priority, String vehicle, String disease)
 	{
+		this.trNr = trNr;
 		this.from = from;
 		this.patient = patient;
 		this.to = to;
 		this.location = location;
+		this.priority = priority;
+		this.vehicle = vehicle;
+		this.disease = disease;
 	}
 
 	/**
@@ -37,6 +45,15 @@ public class TransportViewFilter  extends ViewerFilter
 		//cast to a transport
 		Transport transport = (Transport)element;
 
+		//check the transport number field
+		if(trNr != null &! trNr.trim().isEmpty())
+		{
+			//check the transort number
+			if(!String.valueOf(transport.getTransportNumber()).contains(trNr) &! String.valueOf(transport.getTransportNumber()).startsWith(trNr))
+				return false;
+		}
+		
+		
 		//check the from field
 		if(from != null &! from.trim().isEmpty())
 		{
@@ -61,10 +78,78 @@ public class TransportViewFilter  extends ViewerFilter
 		}
 		if(location != null &! location.trim().isEmpty())
 		{
-			if(!transport.getPlanedLocation().getLocationName().contains(location) &! transport.getPlanedLocation().getLocationName().startsWith(location))
+			if(transport.getVehicleDetail() != null)
+			{
+				if(transport.getVehicleDetail().getCurrentStation() != null)
+				{
+					String locationToCheck = this.getLocationToCheck(transport.getVehicleDetail().getCurrentStation().getLocationName());
+					
+					if(!locationToCheck.contains(location) &! locationToCheck.startsWith(location))
+					return false;
+				}
+			}
+		}
+		if(priority != null &! priority.trim().isEmpty())
+		{
+			String priorityToCheck = this.getPriorityToCheck(transport.getTransportPriority());
+			if(!priorityToCheck.contains(priority) &! priorityToCheck.startsWith(priority))
 			return false;
+		}
+		if(vehicle != null &! vehicle.trim().isEmpty())
+		{
+			if(transport.getVehicleDetail() != null)
+			{
+				if(!transport.getVehicleDetail().getVehicleName().contains(vehicle) &! transport.getVehicleDetail().getVehicleName().startsWith(vehicle))
+				return false;
+			}
+		}
+		if(disease != null &! disease.trim().isEmpty())
+		{
+			if(transport.getKindOfIllness() != null)
+			{
+				if(!transport.getKindOfIllness().getDiseaseName().contains(disease) &! transport.getKindOfIllness().getDiseaseName().startsWith(disease))
+				return false;
+			}
 		}
 		//nothing matched
 		return true;
+	}
+	
+	private String getLocationToCheck(String location)
+	{
+		if(location.equalsIgnoreCase("Kapfenberg"))
+			return "KA";
+		else if(location.equalsIgnoreCase("Bruck an der Mur"))
+			return "BM";
+		else if(location.equalsIgnoreCase("St. Marein"))
+			return "MA";
+		else if(location.equalsIgnoreCase("Breitenau"))
+			return "BR";
+		else if(location.equalsIgnoreCase("Thörl") || location.equalsIgnoreCase("Th?rl"))
+			return "TH";
+		else if(location.equalsIgnoreCase("Turnau"))
+			return "TU";
+		else if(location.equalsIgnoreCase("Bezirk: Bruck - Kapfenberg"))
+			return "BE";
+		return "BE";
+	}
+	
+	private String getPriorityToCheck(String priority)
+	{
+		if(priority.equalsIgnoreCase("A"))
+    		return "1";
+    	else if(priority.equalsIgnoreCase("B"))
+    		return "2";
+    	else if(priority.equalsIgnoreCase("C"))
+    		return "3";
+    	else if(priority.equalsIgnoreCase("D"))
+    		return "4";
+    	else if(priority.equalsIgnoreCase("E"))
+    		return "5";
+    	else if(priority.equalsIgnoreCase("F"))
+    		return "6";
+    	else if(priority.equalsIgnoreCase("G"))
+    		return "7";
+		return "3";
 	}
 }
