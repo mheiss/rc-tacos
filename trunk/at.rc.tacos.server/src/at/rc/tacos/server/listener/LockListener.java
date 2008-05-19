7b17f4f81cb0e81b0da8b6a489bfba38d0ebe7f8
heissm <heissm@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-05-19 11:36:21 +0000

    Locking based on string and integer value to identify a record to lock.

--- trunk/at.rc.tacos.server/src/at/rc/tacos/server/listener/LockListener.java
+++ trunk/at.rc.tacos.server/src/at/rc/tacos/server/listener/LockListener.java
@@ -6,6 +6,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.ListIterator;
 
+import org.apache.log4j.Logger;
+
 import at.rc.tacos.common.AbstractMessage;
 import at.rc.tacos.model.DAOException;
 import at.rc.tacos.model.Lock;
@@ -15,6 +17,9 @@ public class LockListener extends ServerListenerAdapter
 {
 	//the list of locked object
 	private final static List<Lock> lockedList = Collections.synchronizedList(new ArrayList<Lock>());
+	
+	//the logger
+	private static Logger logger = Logger.getLogger(LockListener.class);
 
 	/**
 	 * The request to add a new lock to the managed list of locks
@@ -23,25 +28,25 @@ public class LockListener extends ServerListenerAdapter
 	public AbstractMessage handleAddRequest(AbstractMessage addObject,String username) throws DAOException, SQLException 
 	{
 		Lock lock = (Lock)addObject;
-		System.out.println("New lock request");
+		logger.debug("New lock request: "+lock);
 		//check if the list already contains the lock 
 		if(lockedList.contains(addObject))
 		{
 			int index = lockedList.indexOf(lock);
-			System.out.println("The lock is existing, editing is denied");
+			logger.debug("The lock is existing, editing is denied");
 			//set the username and return the message
 			Lock existingLock = lockedList.get(index);
 			lock.setLockedBy(existingLock.getLockedBy());
 		}
 		else
 		{
-			System.out.println("The lock is not in the list of objects");
+			logger.debug("The lock is not in the list of objects, editing is granted");
 			//set the lock for this user
 			lock.setHasLock(true);
 			lockedList.listIterator().add(lock);
 			return lock;
 		}
-		System.out.println("The lock was not allowed: "+lock);
+		logger.debug("The lock was not allowed: "+lock);
 		//do not allow the user to have the lock
 		lock.setHasLock(false);
 		return lock;
@@ -63,7 +68,7 @@ public class LockListener extends ServerListenerAdapter
 	public AbstractMessage handleRemoveRequest(AbstractMessage removeObject) throws DAOException, SQLException 
 	{
 		Lock lock = (Lock)removeObject;
-		System.out.println("Request to remove the lock:"+lock);
+		logger.debug("Request to remove the lock:"+lock);
 		//remove the lock from the list
 		ListIterator<Lock> iter = lockedList.listIterator();
 		while(iter.hasNext())
@@ -72,7 +77,7 @@ public class LockListener extends ServerListenerAdapter
 			//check the object and remove it from the list
 			if(listLock.equals(lock))
 			{
-				System.out.println("Removing lock:"+lock);
+				logger.debug("Removing lock:"+lock);
 				iter.remove();
 				lock.setHasLock(false);
 				return lock;
