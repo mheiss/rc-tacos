2cfac4d475de77bebe611758307e5491b2430c77
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-05-30 13:52:38 +0000

    table columns

--- trunk/at.rc.tacos.client/src/at/rc/tacos/client/providers/SickPersonLabelProvider.java
+++ trunk/at.rc.tacos.client/src/at/rc/tacos/client/providers/SickPersonLabelProvider.java
@@ -1,41 +1,89 @@
 package at.rc.tacos.client.providers;
 
-import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.ILabelProviderListener;
+import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.swt.graphics.Image;
+
 import at.rc.tacos.model.SickPerson;
 
-public class SickPersonLabelProvider extends LabelProvider 
+public class SickPersonLabelProvider implements ITableLabelProvider
 {
 	String sickPerson;
-    /**
-     * Returns the image to use for this element.
-     * @param object the object to get the image for
-     * @return the image to use
-     */
+	
+	 //define the columns
+    public static final int COLUMN_LASTNAME = 0;
+    public static final int COLUMN_FIRSTNAME = 1;
+    public static final int COLUMN_STREET = 2;
+    public static final int COLUMN_CITY = 3;
+    public static final int COLUMN_SVNR = 4;
+    public static final int COLUMN_NOTES = 5;
+    
+    
     @Override
-    public Image getImage(Object object)
+    public Image getColumnImage(Object element, int columnIndex) 
     {
-        return null;
+		return null;
     }
 
     /**
      * Returns the text to render.
      */
     @Override
-    public String getText(Object object)
+    public String getColumnText(Object element, int columnIndex) 
     {
-    	SickPerson person = (SickPerson)object;
-    	sickPerson = person.getLastName();
-    	if(person.getFirstName() != null)
-    		sickPerson = sickPerson + " " +person.getFirstName();
-    	if(person.getStreetname() != null)
-    		sickPerson = sickPerson + " " +person.getStreetname();
-    	if(person.getCityname() != null)
-    		sickPerson = sickPerson + " " +person.getCityname();
-    	if(person.getSVNR() != null)
-    		sickPerson = sickPerson + " " +person.getSVNR();
-    	if(person.getNotes() != null)
-    		sickPerson = sickPerson + " " +person.getNotes();
-    	return sickPerson;
+    	SickPerson person = (SickPerson)element;
+        
+        switch(columnIndex)
+        {
+	        case COLUMN_LASTNAME: 
+	        	if(person.getLastName() != null)
+	        		return person.getLastName();
+	        	else return null;
+	        case COLUMN_FIRSTNAME:
+	        	if(person.getFirstName() != null)
+	        		return person.getFirstName();
+	        	else return null;
+	        case COLUMN_STREET:
+	        	if(person.getStreetname() != null)
+	        		return person.getStreetname();
+	        	else return null;
+	        case COLUMN_CITY:
+	        	if(person.getCityname() != null)
+	        		return person.getCityname();
+	        	else return null;
+	        case COLUMN_SVNR:
+	        	if(person.getSVNR() != null)
+	        		return person.getSVNR();
+	        	else return null;
+	        case COLUMN_NOTES:
+	        	if(person.getNotes() != null)
+	        		return person.getNotes();  
+        }
+        return null;
+        
     }
+
+	@Override
+	public void addListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isLabelProperty(Object element, String property) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void removeListener(ILabelProviderListener listener) {
+		// TODO Auto-generated method stub
+		
+	}
 }
