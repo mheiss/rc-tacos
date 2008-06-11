4e737694987cee46b49a6b8446560a41fb0c1acc
PayerM <PayerM@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-06-11 20:45:19 +0000

    Implemented Print Roster

--- trunk/at.rc.tacos.web/src/at/rc/tacos/web/controller/PrintRosterController.java
+++ trunk/at.rc.tacos.web/src/at/rc/tacos/web/controller/PrintRosterController.java
@@ -29,185 +29,209 @@ import at.rc.tacos.web.form.RosterEntryContainerListContainer;
 import at.rc.tacos.web.session.UserSession;
 
 /**
- * Roster Controller.
+ * Print Roster Controller
+ * 
  * @author Payer Martin
  * @version 1.0
  */
-public class RosterController extends Controller {
+public class PrintRosterController extends Controller {
 
 	private static final String PARAM_CURRENT_DATE_NAME = "currentDate";
-	
+
 	private static final String PARAM_LOCATION_NAME = "locationId";
 	private static final String PARAM_LOCATION_NO_VALUE = "noValue";
 	private static final String MODEL_LOCATION_NAME = "location";
 	private static final String MODEL_LOCATION_LIST_NAME = "locationList";
-	
+
 	private static final String MODEL_CALENDAR_DEFAULT_DATE_MILLISECONDS_NAME = "calendarDefaultDateMilliseconds";
 	private static final String MODEL_CALENDAR_RANGE_START_NAME = "calendarRangeStart";
 	private static final String MODEL_CALENDAR_RANGE_END_NAME = "calendarRangeEnd";
 	private static final int MODEL_CALENDAR_RANGE_START_OFFSET = 10;
 	private static final int MODEL_CALENDAR_RANGE_END_OFFSET = 1;
-	
+
 	private static final String PARAM_DATE_NAME = "date";
 	private static final String MODEL_DATE_NAME = "date";
 	private static final String MODEL_DATE_DAY_OF_YEAR_NAME = "dateDayOfYear";
 	private static final String MODEL_DATE_MONTH_NAME = "dateMonth";
 	private static final String MODEL_DATE_YEAR_NAME = "dateYear";
-	
+
 	private static final String MODEL_ROSTER_ENTRY_CONTAINER_LIST_CONTAINER = "rosterEntryContainerListContainer";
-	
-	private static final String PARAM_MESSAGE_CODE_NAME = "messageCode";
-	private static final String MODEL_MESSAGE_CODE_NAME = "messageCode";
-	
+
+
 	@Override
 	public Map<String, Object> handleRequest(HttpServletRequest request,
 			HttpServletResponse response, ServletContext context)
 			throws Exception {
-		
+
 		final Map<String, Object> params = new HashMap<String, Object>();
-		
-		final UserSession userSession = (UserSession)request.getSession().getAttribute("userSession");
+
+		final UserSession userSession = (UserSession) request.getSession()
+				.getAttribute("userSession");
 		final WebClient connection = userSession.getConnection();
-		
+
 		// Put current date to parameter to parameter list
 		params.put(PARAM_CURRENT_DATE_NAME, new Date());
-		
+
 		// Location
-		final String paramLocationId = request.getParameter(PARAM_LOCATION_NAME);
+		final String paramLocationId = request
+				.getParameter(PARAM_LOCATION_NAME);
 		int locationId = 0;
-		Location location = userSession.getDefaultFormValues().getDefaultLocation();
-		if (paramLocationId != null && !paramLocationId.equals("")) 
-		{
+		Location location = null;
+		if (paramLocationId != null && !paramLocationId.equals("")) {
 			if (paramLocationId.equalsIgnoreCase(PARAM_LOCATION_NO_VALUE)) {
 				location = null;
 			} else {
 				locationId = Integer.parseInt(paramLocationId);
 			}
 		}
-		final List<AbstractMessage> locationList = connection.sendListingRequest(Location.ID, null);
+		final List<AbstractMessage> locationList = connection
+				.sendListingRequest(Location.ID, null);
 		if (!Location.ID.equalsIgnoreCase(connection.getContentType())) {
-			throw new IllegalArgumentException("Error: Error at connection to Tacos server occoured.");
+			throw new IllegalArgumentException(
+					"Error: Error at connection to Tacos server occoured.");
 		}
 		params.put(MODEL_LOCATION_LIST_NAME, locationList);
-		for (final Iterator<AbstractMessage> itLoactionList = locationList.iterator(); itLoactionList.hasNext();) {
-			final Location l = (Location)itLoactionList.next();
+		for (final Iterator<AbstractMessage> itLoactionList = locationList
+				.iterator(); itLoactionList.hasNext();) {
+			final Location l = (Location) itLoactionList.next();
 			if (l.getId() == locationId) {
 				location = l;
 			}
 		}
 		params.put(MODEL_LOCATION_NAME, location);
-		
-		// Get Date and create calendar for datepicker
-		Date date = userSession.getDefaultFormValues().getDefaultDate();
-		if (date == null) {
-			date = new Date();
-		}
-		final Calendar calendar = Calendar.getInstance();
-		final int rangeStart = calendar.get(Calendar.YEAR) - MODEL_CALENDAR_RANGE_START_OFFSET;
-		final int rangeEnd = calendar.get(Calendar.YEAR) + MODEL_CALENDAR_RANGE_END_OFFSET;
-		
 
-		params.put(MODEL_CALENDAR_DEFAULT_DATE_MILLISECONDS_NAME, date.getTime());
-		
+		// Get Date and create calendar for datepicker
+		Date date = null;
+		final Calendar calendar = Calendar.getInstance();
+		final int rangeStart = calendar.get(Calendar.YEAR)
+				- MODEL_CALENDAR_RANGE_START_OFFSET;
+		final int rangeEnd = calendar.get(Calendar.YEAR)
+				+ MODEL_CALENDAR_RANGE_END_OFFSET;
+
+		params.put(MODEL_CALENDAR_DEFAULT_DATE_MILLISECONDS_NAME, new Date().getTime());
+
 		params.put(MODEL_CALENDAR_RANGE_START_NAME, rangeStart);
 		params.put(MODEL_CALENDAR_RANGE_END_NAME, rangeEnd);
-		
+
 		final Calendar rangeStartCalendar = Calendar.getInstance();
-		rangeStartCalendar.set(Calendar.YEAR, rangeStartCalendar.get(Calendar.YEAR) - MODEL_CALENDAR_RANGE_START_OFFSET);
-		
+		rangeStartCalendar.set(Calendar.YEAR, rangeStartCalendar
+				.get(Calendar.YEAR)
+				- MODEL_CALENDAR_RANGE_START_OFFSET);
+
 		final Calendar rangeEndCalendar = Calendar.getInstance();
-		rangeEndCalendar.set(Calendar.YEAR, rangeEndCalendar.get(Calendar.YEAR) + MODEL_CALENDAR_RANGE_END_OFFSET);
+		rangeEndCalendar.set(Calendar.YEAR, rangeEndCalendar.get(Calendar.YEAR)
+				+ MODEL_CALENDAR_RANGE_END_OFFSET);
 		final String paramDate = request.getParameter(PARAM_DATE_NAME);
-		
+
 		final SimpleDateFormat df = new SimpleDateFormat("dd.MM.yyyy");
-		final SimpleDateFormat formatDateForServer = new SimpleDateFormat("dd-MM-yyyy");
-		
+		final SimpleDateFormat formatDateForServer = new SimpleDateFormat(
+				"dd-MM-yyyy");
+
 		Date dateTemp = null;
 		if (paramDate != null) {
 			try {
 				dateTemp = df.parse(paramDate);
+			} catch (ParseException e) {
+
 			}
-			catch (ParseException e) {
-				
-			}
-			if (dateTemp!= null && dateTemp.getTime() < rangeStartCalendar.getTimeInMillis() || dateTemp.getTime() > rangeEndCalendar.getTimeInMillis()) {
-				//throw new IllegalArgumentException();
+			if (dateTemp != null
+					&& dateTemp.getTime() < rangeStartCalendar
+							.getTimeInMillis()
+					|| dateTemp.getTime() > rangeEndCalendar.getTimeInMillis()) {
+				// throw new IllegalArgumentException();
 			} else {
 				date = dateTemp;
 			}
 		}
+		if (date == null) {
+			throw new IllegalArgumentException("Error: Date must not be null.");
+		}
 		params.put(MODEL_DATE_NAME, date);
-		
+
 		final Calendar c = Calendar.getInstance();
 		c.setTime(date);
 		params.put(MODEL_DATE_DAY_OF_YEAR_NAME, c.get(Calendar.DAY_OF_YEAR));
 		params.put(MODEL_DATE_MONTH_NAME, c.get(Calendar.MONTH));
 		params.put(MODEL_DATE_YEAR_NAME, c.get(Calendar.YEAR));
-				
+
 		final String dateForServerString = formatDateForServer.format(date);
-		
+
 		// Get Roster Entries
 		final QueryFilter rosterFilter = new QueryFilter();
 		rosterFilter.add(IFilterTypes.DATE_FILTER, dateForServerString);
 		if (location != null) {
-			rosterFilter.add(IFilterTypes.ROSTER_LOCATION_FILTER, Integer.toString(location.getId()));
-		}	
+			rosterFilter.add(IFilterTypes.ROSTER_LOCATION_FILTER, Integer
+					.toString(location.getId()));
+		}
 		// Form RosterEntryContainerList for Table
-		final List<AbstractMessage> rosterEntryList = connection.sendListingRequest(RosterEntry.ID, rosterFilter);
+		final List<AbstractMessage> rosterEntryList = connection
+				.sendListingRequest(RosterEntry.ID, rosterFilter);
 		final List<RosterEntryContainer> rosterEntryContainerList = new ArrayList<RosterEntryContainer>();
 		if (!RosterEntry.ID.equalsIgnoreCase(connection.getContentType())) {
-			throw new IllegalArgumentException("Error: Error at connection to Tacos server occoured.");
+			throw new IllegalArgumentException(
+					"Error: Error at connection to Tacos server occoured.");
 		}
-		for (Iterator<AbstractMessage> itRosterEntryList = rosterEntryList.iterator(); itRosterEntryList.hasNext();) {
-			final RosterEntry rosterEntry = (RosterEntry)itRosterEntryList.next();
+		for (Iterator<AbstractMessage> itRosterEntryList = rosterEntryList
+				.iterator(); itRosterEntryList.hasNext();) {
+			final RosterEntry rosterEntry = (RosterEntry) itRosterEntryList
+					.next();
 			final RosterEntryContainer rosterEntryContainer = new RosterEntryContainer();
 			rosterEntryContainer.setRosterEntry(rosterEntry);
-			rosterEntryContainer.setPlannedStartOfWork(new Date(rosterEntry.getPlannedStartOfWork()));
-			rosterEntryContainer.setPlannedEndOfWork(new Date(rosterEntry.getPlannedEndOfWork()));
+			rosterEntryContainer.setPlannedStartOfWork(new Date(rosterEntry
+					.getPlannedStartOfWork()));
+			rosterEntryContainer.setPlannedEndOfWork(new Date(rosterEntry
+					.getPlannedEndOfWork()));
 			if (rosterEntry.getRealStartOfWork() == 0) {
 				rosterEntryContainer.setRealStartOfWork(null);
 			} else {
-				rosterEntryContainer.setRealStartOfWork(new Date(rosterEntry.getRealStartOfWork()));
+				rosterEntryContainer.setRealStartOfWork(new Date(rosterEntry
+						.getRealStartOfWork()));
 			}
 			if (rosterEntry.getRealEndOfWork() == 0) {
 				rosterEntryContainer.setRealEndOfWork(null);
 			} else {
-				rosterEntryContainer.setRealEndOfWork(new Date(rosterEntry.getRealEndOfWork()));
+				rosterEntryContainer.setRealEndOfWork(new Date(rosterEntry
+						.getRealEndOfWork()));
 			}
 			final Calendar deadlineCalendar = Calendar.getInstance();
-			deadlineCalendar.setTime(rosterEntryContainer.getPlannedStartOfWork());
-			deadlineCalendar.set(Calendar.HOUR, deadlineCalendar.get(Calendar.HOUR) - RosterEntryContainer.EDIT_ROSTER_ENTRY_DEADLINE_HOURS);
+			deadlineCalendar.setTime(rosterEntryContainer
+					.getPlannedStartOfWork());
+			deadlineCalendar.set(Calendar.HOUR, deadlineCalendar
+					.get(Calendar.HOUR)
+					- RosterEntryContainer.EDIT_ROSTER_ENTRY_DEADLINE_HOURS);
 			rosterEntryContainer.setDeadline(deadlineCalendar.getTime());
-			
+
 			final Calendar registerStartCalendar = Calendar.getInstance();
-			registerStartCalendar.setTime(rosterEntryContainer.getPlannedStartOfWork());
-			registerStartCalendar.set(Calendar.HOUR, registerStartCalendar.get(Calendar.HOUR) - RosterEntryContainer.REGISTER_ROSTER_ENTRY_DEADLINE_HOURS);
-			rosterEntryContainer.setRegisterStart(registerStartCalendar.getTime());
-			
+			registerStartCalendar.setTime(rosterEntryContainer
+					.getPlannedStartOfWork());
+			registerStartCalendar
+					.set(
+							Calendar.HOUR,
+							registerStartCalendar.get(Calendar.HOUR)
+									- RosterEntryContainer.REGISTER_ROSTER_ENTRY_DEADLINE_HOURS);
+			rosterEntryContainer.setRegisterStart(registerStartCalendar
+					.getTime());
+
 			rosterEntryContainerList.add(rosterEntryContainer);
 		}
-		
+
 		// Group and Sort
-		final RosterEntryContainerListContainer container = new RosterEntryContainerListContainer(rosterEntryContainerList);
-		final Comparator<Location> locationComparator = new PropertyComparator("locationName", true, true);
+		final RosterEntryContainerListContainer container = new RosterEntryContainerListContainer(
+				rosterEntryContainerList);
+		final Comparator<Location> locationComparator = new PropertyComparator(
+				"locationName", true, true);
 		final Comparator sortComp = new CompoundComparator(new Comparator[] {
-				new PropertyComparator("rosterEntry.staffMember.lastName", true, true),
-				new PropertyComparator("rosterEntry.staffMember.firstName", true, true),
-				new PropertyComparator("plannedStartOfWork", true, true)
-		});
+				new PropertyComparator("rosterEntry.staffMember.lastName",
+						true, true),
+				new PropertyComparator("rosterEntry.staffMember.firstName",
+						true, true),
+				new PropertyComparator("plannedStartOfWork", true, true) });
 		container.groupRosterEntriesBy(locationComparator);
 		container.sortRosterEntries(sortComp);
-			
+
 		params.put(MODEL_ROSTER_ENTRY_CONTAINER_LIST_CONTAINER, container);
-		
-		// Parse message code from other controllers
-		if (request.getParameter(PARAM_MESSAGE_CODE_NAME) != null && !request.getParameter(PARAM_MESSAGE_CODE_NAME).equals("")) {
-			params.put(MODEL_MESSAGE_CODE_NAME, request.getParameter(PARAM_MESSAGE_CODE_NAME));
-		}
-		
-		userSession.getDefaultFormValues().setDefaultLocation(location);
-		userSession.getDefaultFormValues().setDefaultDate(date);
-		
-		return params;	
+
+		return params;
 	}
+
 }
---
+++ trunk/at.rc.tacos.web/web/WEB-INF/jsp/printRoster.jsp
@@ -0,0 +1,105 @@
+<%@ include file="includes.jsp" %>
+<c:set var="fieldHeadersRow">
+	<tr class="subhead2">
+		<th nowrap="nowrap">Name</th>
+		<th nowrap="nowrap">von&nbsp;(geplant)</th>
+		<th nowrap="nowrap">bis&nbsp;(geplant)</th>
+		<th nowrap="nowrap">von&nbsp;(real)</th>
+		<th nowrap="nowrap">bis&nbsp;(real)</th>
+		<th nowrap="nowrap">Verwendung</th>
+		<th nowrap="nowrap">Dienstverhältnis</th>
+		<th nowrap="nowrap">Bereitschaft</th>
+	</tr>
+</c:set>
+<c:set var="rosterEntryContainerMap" value="${params.rosterEntryContainerListContainer.rosterEntryContainerMap}"/>
+<c:choose>
+	<c:when test="${fn:length(rosterEntryContainerMap) gt 0}">
+		<table id="rosterEntryTable" class="list" cellpadding="3" cellspacing="0">
+			<c:forEach var="rosterEntryContainerMapEntry" items="${rosterEntryContainerMap}">
+				<c:set var="location" value="${rosterEntryContainerMapEntry.key}"/>	
+				<tr>
+					<th class="header2" colspan="8">${location.locationName}&nbsp;am&nbsp;<fmt:formatDate
+						type="date" dateStyle="medium" value="${params.date}" /></th>
+				</tr>
+				${fieldHeadersRow}
+				<tbody>
+					<c:forEach var="rosterEntryContainer"
+						items="${rosterEntryContainerMapEntry.value}" varStatus="loop">
+						<tr class="${loop.count % 2 == 0 ? 'even' : 'odd'}">
+							<td nowrap="nowrap">${rosterEntryContainer.rosterEntry.staffMember.lastName}
+							${rosterEntryContainer.rosterEntry.staffMember.firstName}</td>
+							<td>
+								<c:choose>
+									<c:when test="${rosterEntryContainer.plannedStartOfWorkDayOfYear ne params.dateDayOfYear or rosterEntryContainer.plannedStartOfWorkMonth ne params.dateMonth or rosterEntryContainer.plannedStartOfWorkYear ne params.dateYear}">
+										<fmt:formatDate type="both" dateStyle="short" timeStyle="short" value="${rosterEntryContainer.plannedStartOfWork}" />
+									</c:when>
+									<c:otherwise>
+										<fmt:formatDate type="time" timeStyle="short" value="${rosterEntryContainer.plannedStartOfWork}" />
+									</c:otherwise>
+								</c:choose>
+							</td>
+							<td>
+								<c:choose>
+									<c:when test="${rosterEntryContainer.plannedEndOfWorkDayOfYear ne params.dateDayOfYear or rosterEntryContainer.plannedEndOfWorkMonth ne params.dateMonth or rosterEntryContainer.plannedEndOfWorkYear ne params.dateYear}">
+										<fmt:formatDate type="both" dateStyle="short" timeStyle="short" value="${rosterEntryContainer.plannedEndOfWork}" />
+									</c:when>
+									<c:otherwise>
+										<fmt:formatDate type="time" timeStyle="short" value="${rosterEntryContainer.plannedEndOfWork}" />
+									</c:otherwise>
+								</c:choose>
+							</td>
+							<td>
+								<c:choose>
+									<c:when test="${rosterEntryContainer.realStartOfWork eq null}">-</c:when>
+									<c:otherwise>
+										<c:choose>
+											<c:when test="${(rosterEntryContainer.realStartOfWorkDayOfYear ne params.dateDayOfYear or rosterEntryContainer.realStartOfWorkMonth ne params.dateMonth or rosterEntryContainer.realStartOfWorkYear ne params.dateYear) and (rosterEntryContainer.realStartOfWorkDayOfYear ne -1 and rosterEntryContainer.realStartOfWorkMonth ne -1 and rosterEntryContainer.realStartOfWorkYear ne -1)}">
+												<fmt:formatDate type="both" dateStyle="short" timeStyle="short" value="${rosterEntryContainer.realStartOfWork}" />
+											</c:when>
+											<c:otherwise>
+												<fmt:formatDate type="time" timeStyle="short" value="${rosterEntryContainer.realStartOfWork}" />
+											</c:otherwise>
+										</c:choose>
+									</c:otherwise>
+								</c:choose>
+							</td>
+							<td>
+								<c:choose>
+									<c:when test="${rosterEntryContainer.realEndOfWork eq null}">-</c:when>
+									<c:otherwise>
+										<c:choose>
+											<c:when test="${(rosterEntryContainer.realEndOfWorkDayOfYear ne params.dateDayOfYear or rosterEntryContainer.realEndOfWorkMonth ne params.dateMonth or rosterEntryContainer.realEndOfWorkYear ne params.dateYear) and (rosterEntryContainer.realEndOfWorkDayOfYear ne -1 and rosterEntryContainer.realEndOfWorkMonth ne -1 and rosterEntryContainer.realEndOfWorkYear ne -1)}">
+												<fmt:formatDate type="both" dateStyle="short" timeStyle="short" value="${rosterEntryContainer.realEndOfWork}" />
+											</c:when>
+											<c:otherwise>
+												<fmt:formatDate type="time" timeStyle="short" value="${rosterEntryContainer.realEndOfWork}" />
+											</c:otherwise>
+										</c:choose>
+									</c:otherwise>
+								</c:choose>
+							</td>
+							<td nowrap="nowrap">${rosterEntryContainer.rosterEntry.job.jobName}</td>
+							<td nowrap="nowrap">${rosterEntryContainer.rosterEntry.servicetype.serviceName}</td>
+							<td nowrap="nowrap"><c:choose>
+								<c:when
+									test="${rosterEntryContainer.rosterEntry.standby eq true}">Ja</c:when>
+								<c:otherwise>Nein</c:otherwise>
+							</c:choose></td>
+						</tr>
+					</c:forEach>
+				</tbody>
+			</c:forEach>
+		</table>
+	</c:when>
+	<c:otherwise>
+		<table cellpadding="3" cellspacing="0" class="list">
+			<tr>
+				<td class="nodata">Keine&nbsp;Treffer</td>
+			</tr>
+		</table>
+	</c:otherwise>
+</c:choose>
+<br />
+<br />
+<c:url var="url" value="/Dispatcher/roster.do" />
+<a href="${url}">Zur&uuml;ck</a>
