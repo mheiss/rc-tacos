1f3f969827432070a5b8a93e281ae3ff57fee611
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-06-12 18:23:44 +0000

    new filter type added

--- trunk/at.rc.tacos.model/src/at/rc/tacos/common/IFilterTypes.java
+++ trunk/at.rc.tacos.model/src/at/rc/tacos/common/IFilterTypes.java
@@ -77,4 +77,6 @@ public interface IFilterTypes
     public final static String TRANSPORT_LOCATION_FILTER = "transportLocationFilter";
     
     public final static String SERVICETYPE_COMPETENCE_FILTER = "serviceTypeCompetenceFilter";
+    
+    public final static String TRANSPORT_UNDERWAY_FILTER = "transportUnderwayFilter";
 }
