f0ebe9e0219612e0e208620b9e97d3cd4a9b80ff
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2008-06-22 01:55:05 +0000

    phone handling changed for staff members

--- trunk/at.rc.tacos.model/src/at/rc/tacos/model/StaffMember.java
+++ trunk/at.rc.tacos.model/src/at/rc/tacos/model/StaffMember.java
@@ -27,6 +27,8 @@ public class StaffMember extends AbstractMessage
 	private List<Competence> competenceList;
 	private String eMail;
 	private String userName;
+	private String phone1;
+	private String phone2;
 	
 	//internal information, only needed to serialize and deserialize
 	public String function;
@@ -376,4 +378,24 @@ public class StaffMember extends AbstractMessage
     {
         competenceList.add(competence);
     }
+
+	public String getPhone1() 
+	{
+		return phone1;
+	}
+
+	public void setPhone1(String phone1) 
+	{
+		this.phone1 = phone1;
+	}
+
+	public String getPhone2() 
+	{
+		return phone2;
+	}
+
+	public void setPhone2(String phone2) 
+	{
+		this.phone2 = phone2;
+	}
 }
