771faece1fdc28c5bf39ab8116106534c542c33c
heissm <michael.heiss@outlook.at>
2008-10-06 21:16:49 +0000

    added two basic test cases for writing xml

---
+++ sample/simpleXml/src/simplexml/test/XStreamTests.java
@@ -0,0 +1,30 @@
+package simplexml.test;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+import org.junit.Test;
+
+import com.thoughtworks.xstream.XStream;
+
+import simplexml.model.Message;
+
+/**
+ * XStream test cases to generate xml from java and vice versa.
+ * @author Michael
+ *
+ */
+public class XStreamTests {
+	
+	@Test
+	public void testMarshalMessage() throws FileNotFoundException {
+		Message message = new Message("add","myClass");
+		message.addParam("param1", "valueq");
+		
+		//create a new marshalling factory for message
+		XStream xStream = new XStream();
+		xStream.alias("message", Message.class);
+		xStream.toXML(message, new FileOutputStream("gen-data/xstream_message.xml"));
+	}
+
+}
