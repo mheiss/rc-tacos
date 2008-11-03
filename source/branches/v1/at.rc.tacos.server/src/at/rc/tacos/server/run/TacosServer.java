661677f090e8eedbf18f901e4fefb0e867b0823f
heissm <michael.heiss@outlook.at>
2008-11-03 21:54:52 +0000

    Some minor changes to start the server as a service using JavaServiceWrapper.

--- source/branches/v1/at.rc.tacos.server/src/at/rc/tacos/server/run/TacosServer.java
+++ source/branches/v1/at.rc.tacos.server/src/at/rc/tacos/server/run/TacosServer.java
@@ -1,10 +1,6 @@
 package at.rc.tacos.server.run;
 
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
-
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 
 import at.rc.tacos.core.db.DataSource;
 import at.rc.tacos.core.net.internal.MyServer;
@@ -12,81 +8,67 @@ import at.rc.tacos.server.controller.ClientHandler;
 import at.rc.tacos.server.controller.ServerController;
 
 /**
- * Tacos server
+ * Tacos server main entry point to startup the application from the command
+ * line.
+ * 
  * @author Michael
  */
-public class TacosServer
-{
-    //path for the properties file
-    public final static String SERVER_CONFIG = "at.rc.tacos.server.config.server";
-    //the logging instance
-    private static Logger logger = Logger.getRootLogger();
+public class TacosServer {
+	// the logging instance
+	private static Logger logger = Logger.getRootLogger();
 
-    //the server object
-    private MyServer myServer = null;
+	// the server object
+	private MyServer myServer = null;
 
-    public TacosServer(int port)
-    {
-        ServerController.getDefault();
-        //listen for client request
-        myServer = new MyServer(port);
-        myServer.addNetListener(new ClientHandler());
-    }
-    
-    /**
-     * Starts the server thread to listen for client requests.
-     */
-    private void startServer()
-    {
-        //start the server thread to listen to client connections
-        Thread t = new Thread(myServer);
-        t.start();
-    }
+	public TacosServer(int port) {
+		ServerController.getDefault();
+		// listen for client request
+		myServer = new MyServer(port);
+		myServer.addNetListener(new ClientHandler());
+	}
 
-    /**
-     * The main method to read the configuration and start the server
-     * @param args - not used
-     */
-    public static void main(String[] args)  
-    {  
-        try
-        {
-        	PropertyConfigurator.configureAndWatch("conf/log4j.properties", 60*1000 );
-            //load the settings from the file
-            String strPort = ResourceBundle.getBundle(SERVER_CONFIG).getString("server.port");
-            int port = -1;
-            //parse
-            port = Integer.parseInt(strPort);
-            //start the server
-            logger.info("TACOS-Server Build: 05.06.2008"); 
-            logger.info("Open a connection to the database server");
-            //try to get a connection to the database
-            if(DataSource.getInstance().getConnection() == null)
-            {
-            	logger.error("Failed to connect to the database");
-            	logger.error("Shuting down the server");
-                System.exit(1);
-            }
-            logger.info("Listening for client request at port: "+port);
-            TacosServer server = new TacosServer(port);
-            server.startServer();
-            
-        }
-        catch(MissingResourceException mre)
-        {
-        	logger.error("Missing resource, cannot init startup server");
-        	logger.error(mre.getMessage());
-        }
-        catch(NumberFormatException nfe)
-        {
-        	logger.info("Port number must be a integer");
-        	logger.info(nfe.getMessage());
-        }
-        catch(NullPointerException npe)
-        {
-        	logger.info("Configuration file for the server is missing");
-        	logger.info(npe.getMessage());
-            npe.printStackTrace();
-        }
-    }
+	/**
+	 * Starts the server thread to listen for client requests.
+	 */
+	private void startServer() {
+		// start the server thread to listen to client connections
+		Thread t = new Thread(myServer);
+		t.start();
+	}
+
+	/**
+	 * The main method to read the configuration and start the server
+	 * 
+	 * @param args -
+	 *            not used
+	 */
+	public static void main(String[] args) {
+		int port;
+
+		try {
+			// the first command line argument is the port
+			if (args.length != 1) {
+				throw new IllegalArgumentException(
+						"The first argument must be the port where the server should listen to");
+			}
+			port = Integer.parseInt(args[0]);
+
+			// start the server
+			logger.info("TACOS-Server Build: 03.11.2008");
+			logger.info("Open a connection to the database server");
+			// try to get a connection to the database
+			if (DataSource.getInstance().getConnection() == null) {
+				logger.error("Failed to connect to the database");
+				logger.error("Shuting down the server");
+				System.exit(1);
+			}
+			logger.info("Listening for client request at port: " + port);
+			TacosServer server = new TacosServer(port);
+			server.startServer();
+
+		} catch (NumberFormatException nfe) {
+			logger.info("Port number must be a integer");
+			logger.info(nfe.getMessage());
+		}
+	}
 }
