cbfc0c31f5033fa6e120d619ad02ac371e32e0b7
heissm <heissm@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2009-03-01 15:17:20 +0000

    Bugfix: Some roster entries are not displayed in the personal view.

--- source/branches/v1/at.rc.tacos.client/src/at/rc/tacos/client/providers/PersonalDateFilter.java
+++ source/branches/v1/at.rc.tacos.client/src/at/rc/tacos/client/providers/PersonalDateFilter.java
@@ -1,6 +1,8 @@
 package at.rc.tacos.client.providers;
 
 import java.util.Calendar;
+
+import org.apache.commons.lang.time.DateUtils;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 
@@ -8,73 +10,56 @@ import at.rc.tacos.model.RosterEntry;
 
 /**
  * Filters out all transports that didnt't match the current date
+ * 
  * @author Birgit
  */
-public class PersonalDateFilter extends ViewerFilter
-{
-	//properties
-	private int filterDay;
-	private int filterMonth;
-	private int filterYear;
+public class PersonalDateFilter extends ViewerFilter {
+
+	private Calendar filter;
 
 	/**
 	 * Default class constructor specifying the date for the filter
 	 */
-	public PersonalDateFilter(Calendar filterCalendar)
-	{
-		this.filterDay = filterCalendar.get(Calendar.DAY_OF_MONTH);
-		this.filterMonth = filterCalendar.get(Calendar.MONTH);
-		this.filterYear = filterCalendar.get(Calendar.YEAR);
+	public PersonalDateFilter(Calendar filter) {
+		this.filter = DateUtils.truncate(filter, Calendar.DAY_OF_MONTH);
 	}
-	
+
 	/**
 	 * Returns whether or not the object should be filtered out.
-	 * @param viewer the viewer
-	 * @param parentElement the parent element
-	 * @param element the element to check
+	 * 
+	 * @param viewer
+	 *            the viewer
+	 * @param parentElement
+	 *            the parent element
+	 * @param element
+	 *            the element to check
 	 */
 	@Override
-	public boolean select(Viewer viewer, Object parentElement, Object element) 
-	{
-		//the values of the roster entry
-		int startDay,startMonth,startYear;
-		int endDay,endMonth, endYear;
-		
-		RosterEntry entry = (RosterEntry)element;
-		
-		//init the start values
-		Calendar entryCal = Calendar.getInstance();
-		entryCal.setTimeInMillis(entry.getPlannedStartOfWork());
-		startDay = entryCal.get(Calendar.DAY_OF_MONTH);
-		startMonth = entryCal.get(Calendar.MONTH);
-		startYear = entryCal.get(Calendar.YEAR);
-		
-		//init the end values
-		entryCal.setTimeInMillis(entry.getPlannedEndOfWork());
-		endDay = entryCal.get(Calendar.DAY_OF_MONTH);
-		endMonth = entryCal.get(Calendar.MONTH);
-		endYear = entryCal.get(Calendar.YEAR);
-		
-		//if we do not have a split entry then the date must match
-		if(!entry.isSplitEntry())
-		{
-			if(startDay != filterDay)
-				return false;
-			if(startMonth != filterMonth)
-				return false;
-			if(startYear != filterYear)
-				return false;
-			//the dates math so show the entry
-			return true;
+	public boolean select(Viewer viewer, Object parentElement, Object element) {
+		RosterEntry entry = (RosterEntry) element;
+
+		// init the start values
+		Calendar start = Calendar.getInstance();
+		start.setTimeInMillis(entry.getPlannedStartOfWork());
+		start = DateUtils.truncate(start, Calendar.DAY_OF_MONTH);
+
+		// init the end values
+		Calendar end = Calendar.getInstance();
+		end.setTimeInMillis(entry.getPlannedEndOfWork());
+		end = DateUtils.truncate(end, Calendar.DAY_OF_MONTH);
+
+		// if we do not have a split entry then the date must match
+		if (!entry.isSplitEntry()) {
+			return DateUtils.isSameDay(filter, start);
 		}
-		
-		//for a split entry the start and the end must match
-		if(filterDay != startDay && filterDay != endDay &! (filterDay > startDay && filterDay < endDay))
+
+		// if the entry is split up over more than one day, then check if the filter date is between
+		if(filter.before(start)) {
 			return false;
-		if(filterMonth != startMonth && filterMonth != endMonth)
+		}
+		if(filter.after(end)) {
 			return false;
-		if(filterYear != startYear && filterYear != endYear)
-			return false;
+		}
 		
 		return true;
 	}
