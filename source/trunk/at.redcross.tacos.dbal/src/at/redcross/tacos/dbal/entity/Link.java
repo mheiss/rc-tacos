8b820ecad38f821958fc958baed66c59b5417768
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2010-07-19 20:24:33 +0000

    Link - Class and Stage added

---
+++ source/trunk/at.redcross.tacos.dbal/src/at/redcross/tacos/dbal/entity/Link.java
@@ -0,0 +1,90 @@
+package at.redcross.tacos.dbal.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
+@Entity
+@Table(name = "Link")
+public class Link extends EntityImpl {
+
+	private static final long serialVersionUID = 7889627167234058796L;
+
+	@Id
+    @GeneratedValue
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(length = 255)
+    private String description;
+
+    // ---------------------------------
+    // EntityImpl
+    // ---------------------------------
+    @Override
+    public String getDisplayString() {
+        return name;
+    }
+
+    // ---------------------------------
+    // Object related methods
+    // ---------------------------------
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("name", id).toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(id).hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj == this) {
+            return true;
+        }
+        if (obj.getClass() != getClass()) {
+            return false;
+        }
+        Link rhs = (Link) obj;
+        return new EqualsBuilder().append(id, rhs.id).isEquals();
+    }
+
+    // ---------------------------------
+    // Setters for the properties
+    // ---------------------------------
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // ---------------------------------
+    // Getters for the properties
+    // ---------------------------------
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
