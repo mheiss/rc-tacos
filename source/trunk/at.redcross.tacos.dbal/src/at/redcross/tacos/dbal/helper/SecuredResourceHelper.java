5d153ae412451579efaf0650f2ca3ee184add5f1
heissm <michael.heiss@outlook.at>
2010-08-08 16:52:55 +0000

    Link visibility based on the current authentication

--- source/trunk/at.redcross.tacos.dbal/src/at/redcross/tacos/dbal/helper/SecuredResourceHelper.java
+++ source/trunk/at.redcross.tacos.dbal/src/at/redcross/tacos/dbal/helper/SecuredResourceHelper.java
@@ -9,6 +9,12 @@ import at.redcross.tacos.dbal.entity.SecuredResource;
 
 public class SecuredResourceHelper {
 
+	public static List<SecuredResource> list(EntityManager manager) {
+		String hqlQuery = "from SecuredResource sr";
+		TypedQuery<SecuredResource> query = manager.createQuery(hqlQuery, SecuredResource.class);
+		return query.getResultList();
+	}
+
 	public static List<SecuredResource> getByName(EntityManager manager, String resource) {
 		String hqlQuery = "from SecuredResource sr where sr.resource = :resource";
 		TypedQuery<SecuredResource> query = manager.createQuery(hqlQuery, SecuredResource.class);
--- source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/SecuredResourceBean.java
+++ source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/SecuredResourceBean.java
@@ -1,35 +1,126 @@
 package at.redcross.tacos.web.beans;
 
-import javax.faces.bean.ManagedBean;
+import java.util.List;
 
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.expression.EvaluationContext;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.expression.spel.support.StandardEvaluationContext;
+import org.springframework.security.access.expression.ExpressionUtils;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import at.redcross.tacos.web.security.WebUserDetails;
+import at.redcross.tacos.dbal.entity.SecuredResource;
+import at.redcross.tacos.dbal.helper.SecuredResourceHelper;
+import at.redcross.tacos.dbal.manager.EntityManagerHelper;
+import at.redcross.tacos.web.persitence.EntityManagerFactory;
+import at.redcross.tacos.web.security.WebSecurityExpressionRoot;
+
+import com.ocpsoft.pretty.PrettyContext;
+import com.ocpsoft.pretty.config.PrettyConfig;
+import com.ocpsoft.pretty.config.PrettyUrlMapping;
 
 @ManagedBean(name = "securedResourceBean")
 public class SecuredResourceBean {
 
-	public boolean isAdmin() {
-		return hasGroup("ROLE_ADMIN");
+	private final static Logger log = LoggerFactory.getLogger(SecuredResourceBean.class);
+
+	private List<SecuredResource> cachedResources;
+	private ExpressionParser parser = new SpelExpressionParser();
+
+	@PostConstruct
+	protected void init() {
+		EntityManager manager = null;
+		try {
+			manager = EntityManagerFactory.createEntityManager();
+			cachedResources = SecuredResourceHelper.list(manager);
+		} finally {
+			manager = EntityManagerHelper.close(manager);
+		}
 	}
 
-	/** Returns whether or not the current user is in the specified group */
-	private boolean hasGroup(String... groups) {
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		// anonymous login does only have a java.lang.String principal
-		if (!(auth.getPrincipal() instanceof WebUserDetails)) {
+	// ---------------------------------
+	// Resource request links
+	// ---------------------------------
+	public boolean isAdminLinkVisible() {
+		return canAccess("admin-home");
+	}
+
+	// ---------------------------------
+	// Helper methods
+	// ---------------------------------
+	/** Returns whether or not the current user can access the given URL */
+	private boolean canAccess(String prettyMappingId) {
+		String mappedUrl = findMappingById(prettyMappingId);
+		if (mappedUrl == null) {
+			log.error("Mapping for '" + prettyMappingId + "' not found");
 			return false;
 		}
-		WebUserDetails details = (WebUserDetails) auth.getPrincipal();
-		for (String group : groups) {
-			for (GrantedAuthority grantedAuthority : details.getAuthorities()) {
-				if (grantedAuthority.getAuthority().equals(group)) {
-					return true;
-				}
+		Expression expression = getExpressionByUrl(mappedUrl);
+		if (expression == null) {
+			log.error("Expression for '" + mappedUrl + "' not found");
+			return false;
+		}
+		return canAccess(expression);
+	}
+
+	/** Returns the first matching resource based on the given page */
+	private String findMappingById(String prettyMappingId) {
+		PrettyConfig prettyConfig = PrettyContext.getCurrentInstance().getConfig();
+		for (PrettyUrlMapping mapping : prettyConfig.getMappings()) {
+			if (mapping.getId().equals(prettyMappingId)) {
+				return mapping.getPattern();
 			}
 		}
-		return false;
+		return null;
+	}
+
+	/** Finds the according expression based on the resource */
+	private Expression getExpressionByUrl(String urlResource) {
+		SecuredResource resource = getResourceByUrl(urlResource);
+		if (resource != null) {
+			return parser.parseExpression(resource.getAccess());
+		}
+
+		// go through all parts of the URL
+		String tmp = urlResource;
+		if (!tmp.endsWith("/")) {
+			tmp = tmp + "/";
+		}
+		while (!tmp.isEmpty()) {
+			tmp = tmp.substring(0, tmp.lastIndexOf("/"));
+			resource = getResourceByUrl(tmp + "/**");
+			if (resource != null) {
+				return parser.parseExpression(resource.getAccess());
+			}
+		}
+		return null;
+	}
+
+	/** Finds the best matching secured resource according to the given URL */
+	private SecuredResource getResourceByUrl(String url) {
+		for (SecuredResource resource : cachedResources) {
+			if (resource.getResource().equals(url)) {
+				return resource;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns whether or not the user can access the given resource
+	 */
+	private boolean canAccess(Expression expression) {
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		WebSecurityExpressionRoot root = new WebSecurityExpressionRoot(auth);
+		EvaluationContext ctx = new StandardEvaluationContext(root);
+		return ExpressionUtils.evaluateAsBoolean(expression, ctx);
 	}
 }
