e55db2100f664e11217748f83cf3e2281db8f6b2
heissm <heissm@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2010-08-09 20:42:52 +0000

    Competence and Group management added.

--- source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/UserMaintenanceBean.java
+++ source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/UserMaintenanceBean.java
@@ -1,9 +1,11 @@
 package at.redcross.tacos.web.beans;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
+import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
 import javax.persistence.EntityManager;
 
@@ -13,10 +15,13 @@ import org.springframework.security.authentication.encoding.ShaPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import at.redcross.tacos.dbal.entity.Address;
+import at.redcross.tacos.dbal.entity.Competence;
 import at.redcross.tacos.dbal.entity.Gender;
+import at.redcross.tacos.dbal.entity.Group;
 import at.redcross.tacos.dbal.entity.Login;
 import at.redcross.tacos.dbal.entity.SystemUser;
 import at.redcross.tacos.dbal.helper.CompetenceHelper;
+import at.redcross.tacos.dbal.helper.GroupHelper;
 import at.redcross.tacos.dbal.helper.LocationHelper;
 import at.redcross.tacos.dbal.manager.EntityManagerHelper;
 import at.redcross.tacos.web.faces.FacesUtils;
@@ -43,10 +48,18 @@ public class UserMaintenanceBean extends BaseBean {
 	private List<SelectItem> genderItems;
 	private List<SelectItem> competenceItems;
 	private List<SelectItem> locationItems;
+	private List<SelectItem> groupItems;
 
 	/** passwords -> not directly attached to entity */
 	private String password;
 	private String password2;
+
+	/** selected values */
+	private Group selectedGroup;
+	private Competence selectedCompetence;
+
+	private long selectedGroupId;
+	private long selectedCompetenceId;
 
 	/** Encode passwords using SHA */
 	private transient PasswordEncoder encoder;
@@ -59,13 +72,13 @@ public class UserMaintenanceBean extends BaseBean {
 			loadfromDatabase(manager, userId);
 			competenceItems = DropDownHelper.convertToItems(CompetenceHelper.list(manager));
 			locationItems = DropDownHelper.convertToItems(LocationHelper.list(manager));
+			groupItems = DropDownHelper.convertToItems(GroupHelper.list(manager));
 			genderItems = new ArrayList<SelectItem>();
 			genderItems.add(new DropDownItem("männlich", Gender.MALE).getItem());
 			genderItems.add(new DropDownItem("weiblich", Gender.FEMALE).getItem());
 			genderItems.add(new DropDownItem("unbekannt", Gender.UNKNOWN).getItem());
 			encoder = new ShaPasswordEncoder(256);
-		}
-		finally {
+		} finally {
 			manager = EntityManagerHelper.close(manager);
 		}
 	}
@@ -73,9 +86,7 @@ public class UserMaintenanceBean extends BaseBean {
 	// ---------------------------------
 	// Actions
 	// ---------------------------------
-	/**
-	 * Persists the current entity in the database
-	 */
+	/** Persists the current entity in the database */
 	public String persist() {
 		EntityManager manager = null;
 		try {
@@ -89,39 +100,82 @@ public class UserMaintenanceBean extends BaseBean {
 			if (isNew()) {
 				manager.persist(login);
 				manager.persist(systemUser);
-			}
-			else {
+			} else {
 				manager.merge(login);
 				manager.merge(systemUser);
 			}
 			EntityManagerHelper.commit(manager);
 			return FacesUtils.pretty("admin-userOverview");
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			FacesUtils.addErrorMessage("Der Mitarbeitereintrag konnte nicht gespeichert werden");
 			return null;
-		}
-		finally {
+		} finally {
 			manager = EntityManagerHelper.close(manager);
 		}
 	}
 
-	/**
-	 * Reverts any changes that may have been done
-	 */
+	/** Reverts any changes that may have been done */
 	public String revert() {
 		EntityManager manager = null;
 		try {
 			manager = EntityManagerFactory.createEntityManager();
 			loadfromDatabase(manager, systemUser.getId());
 			return FacesUtils.pretty("admin-userEditMaintenance");
-		}
-		catch (Exception ex) {
+		} catch (Exception ex) {
 			FacesUtils.addErrorMessage("Der Mitarbeitereintrag konnte nicht zurückgesetzt werden");
 			return null;
-		}
-		finally {
+		} finally {
 			manager = EntityManagerHelper.close(manager);
+		}
+	}
+
+	/** Adds the currently selected group to this user */
+	public void addGroup(ActionEvent event) {
+		// silently ignore invalid requests
+		if (selectedGroup == null) {
+			return;
+		}
+		List<Group> groups = systemUser.getGroups();
+		if (groups.contains(selectedGroup)) {
+			return;
+		}
+		groups.add(selectedGroup);
+		selectedGroup = null;
+	}
+
+	/** Adds the currently selected competence to this user */
+	public void addCompetence(ActionEvent event) {
+		// silently ignore invalid requests
+		if (selectedCompetence == null) {
+			return;
+		}
+		List<Competence> competences = systemUser.getCompetences();
+		if (competences.contains(selectedCompetence)) {
+			return;
+		}
+		competences.add(selectedCompetence);
+		selectedCompetence = null;
+	}
+
+	/** Removes the currently selected group from this user */
+	public void removeGroup(ActionEvent event) {
+		Iterator<Group> groupIter = systemUser.getGroups().iterator();
+		while (groupIter.hasNext()) {
+			Group group = groupIter.next();
+			if (group.getId() == selectedGroupId) {
+				groupIter.remove();
+			}
+		}
+	}
+
+	/** Adds the currently selected competence to this user */
+	public void removeCompetence(ActionEvent event) {
+		Iterator<Competence> comptIter = systemUser.getCompetences().iterator();
+		while (comptIter.hasNext()) {
+			Competence competence = comptIter.next();
+			if (competence.getId() == selectedCompetenceId) {
+				comptIter.remove();
+			}
 		}
 	}
 
@@ -156,6 +210,22 @@ public class UserMaintenanceBean extends BaseBean {
 		this.password2 = password2;
 	}
 
+	public void setSelectedGroup(Group selectedGroup) {
+		this.selectedGroup = selectedGroup;
+	}
+
+	public void setSelectedCompetence(Competence selectedCompetence) {
+		this.selectedCompetence = selectedCompetence;
+	}
+
+	public void setSelectedCompetenceId(long selectedCompetenceId) {
+		this.selectedCompetenceId = selectedCompetenceId;
+	}
+
+	public void setSelectedGroupId(long selectedGroupId) {
+		this.selectedGroupId = selectedGroupId;
+	}
+
 	// ---------------------------------
 	// Getters for the properties
 	// ---------------------------------
@@ -179,12 +249,24 @@ public class UserMaintenanceBean extends BaseBean {
 		return genderItems;
 	}
 
+	public List<SelectItem> getGroupItems() {
+		return groupItems;
+	}
+
 	public Login getLogin() {
 		return login;
 	}
 
 	public SystemUser getSystemUser() {
 		return systemUser;
+	}
+
+	public Group getSelectedGroup() {
+		return selectedGroup;
+	}
+
+	public Competence getSelectedCompetence() {
+		return selectedCompetence;
 	}
 
 	public String getPassword() {
