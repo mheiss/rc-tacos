e0ef88a4f93db33d7ab99442b8290962ebd389b5
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2010-08-19 22:11:55 +0000

    show links

---
+++ source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/LinkOverviewBean.java
@@ -0,0 +1,53 @@
+package at.redcross.tacos.web.beans;
+
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.persistence.EntityManager;
+
+import org.ajax4jsf.model.KeepAlive;
+
+import at.redcross.tacos.dbal.entity.Link;
+import at.redcross.tacos.dbal.helper.LinkHelper;
+import at.redcross.tacos.dbal.manager.EntityManagerHelper;
+import at.redcross.tacos.web.persitence.EntityManagerFactory;
+
+@KeepAlive
+@ManagedBean(name = "linkOverviewBean")
+public class LinkOverviewBean extends BaseBean {
+
+	private static final long serialVersionUID = 1L;
+
+
+    /** queried results for visualization / reporting */
+    private List<Link> links;
+
+    @Override
+    protected void init() throws Exception {
+        EntityManager manager = null;
+        try {
+            manager = EntityManagerFactory.createEntityManager();
+            loadFromDatabase(manager);
+        }
+        finally {
+            manager = EntityManagerHelper.close(manager);
+        }
+    }
+
+
+    // ---------------------------------
+    // Private API
+    // ---------------------------------
+    private void loadFromDatabase(EntityManager manager) {
+        links = LinkHelper.list(manager);
+    }
+
+
+    // ---------------------------------
+    // Getters for the properties
+    // ---------------------------------
+    public List<Link> getLinks() {
+        return links;
+    }
+    
+}
