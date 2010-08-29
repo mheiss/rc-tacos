e88a5242e38ff5de4d7d80b149636cd923d0b311
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2010-08-29 12:23:34 +0000

    Info and category added

---
+++ source/trunk/at.redcross.tacos.datasetup/src/at/redcross/tacos/datasetup/stage1/CategoryStage.java
@@ -0,0 +1,36 @@
+package at.redcross.tacos.datasetup.stage1;
+
+import javax.persistence.EntityManager;
+
+import javax.persistence.TypedQuery;
+
+import at.redcross.tacos.datasetup.DatasetupStage;
+import at.redcross.tacos.dbal.entity.Category;
+
+// creates assignment stages
+public class CategoryStage implements DatasetupStage {
+
+    @Override
+    public void performCleanup(EntityManager manager) {
+        TypedQuery<Category> query = manager.createQuery("from Category", Category.class);
+        for (Category category : query.getResultList()) {
+            manager.remove(category);
+        }
+    }
+
+    @Override
+    public void performImport(EntityManager manager) {
+        {
+        	Category category = new Category();
+        	category.setName("Ausbildung");
+        	category.setDescription("Hier werden ausbildungsrelevante Themen angekündigt (Pflichtfortbildungen, (Re-) Zertifizierungen");
+        	manager.persist(category);
+        }
+        {
+        	Category category = new Category();
+        	category.setName("Ambulanzdienst");
+        	category.setDescription("Ambulanzdienste aller Art... ");
+        	manager.persist(category);
+        }
+    }
+}
