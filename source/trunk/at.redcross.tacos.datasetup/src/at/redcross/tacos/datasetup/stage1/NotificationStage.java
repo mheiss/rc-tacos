bf17173ce813ded4c7c85b069a79e26ee0e56466
thekbi <thekbi@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2010-09-23 19:04:26 +0000

    notification for roster car allocation added (started...)

---
+++ source/trunk/at.redcross.tacos.datasetup/src/at/redcross/tacos/datasetup/stage1/NotificationStage.java
@@ -0,0 +1,32 @@
+package at.redcross.tacos.datasetup.stage1;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import at.redcross.tacos.datasetup.DatasetupStage;
+import at.redcross.tacos.dbal.entity.Notification;
+
+// creates notification stages
+public class NotificationStage implements DatasetupStage {
+
+    @Override
+    public void performCleanup(EntityManager manager) {
+        TypedQuery<Notification> query = manager.createQuery("from Notification", Notification.class);
+        for (Notification notification : query.getResultList()) {
+            manager.remove(notification);
+        }
+    }
+
+    @Override
+    public void performImport(EntityManager manager) {
+        {
+            Notification notification = new Notification();
+            Date today = Calendar.getInstance().getTime();
+            notification.setDate(today);
+            notification.setNotes("Anmerkungen zu heute");
+        }
+    }
+}
