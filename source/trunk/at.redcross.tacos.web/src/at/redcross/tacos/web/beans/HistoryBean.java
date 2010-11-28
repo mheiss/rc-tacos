fa9bc1036624dde0092eba35aafd7820d3838692
heissm <heissm@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2010-11-28 21:15:51 +0000

    History updates

--- source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/HistoryBean.java
+++ source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/HistoryBean.java
@@ -1,9 +1,13 @@
 package at.redcross.tacos.web.beans;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.event.ActionEvent;
 import javax.persistence.EntityManager;
@@ -27,197 +31,197 @@ import at.redcross.tacos.web.persistence.EntityManagerFactory;
 
 @KeepAlive
 @ManagedBean(name = "historyBean")
-public class HistoryBean extends BaseBean {
+public class HistoryBean extends PagingBean {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    /** the primary key of the entity to show the revision */
-    private Long primaryKey;
+	/** the primary key of the entity to show the revision */
+	private Long primaryKey;
 
-    /** the class to show the history information */
-    private String className;
+	/** the class to show the history information */
+	private String className;
 
-    /** the entity for that the history is shown */
-    private EntityImpl entity;
+	/** the entity for that the history is shown */
+	private EntityImpl entity;
 
-    /** the history entries */
-    private List<RevisionInfoEntry> revisionEntries;
+	/** the history entries */
+	private List<RevisionInfoEntry> revisionEntries;
 
-    /** the current page */
-    private Integer currentPage = Integer.valueOf(0);
+	/** format dates using the provided formatter */
+	private SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 
-    @Override
-    protected void init() throws Exception {
-        // nothing to do
-    }
+	@PostConstruct
+	public void setup() {
+		maxResults = 10;
+	}
 
-    public void queryHistory(ActionEvent event) {
-        Class<?> entityClass;
-        EntityManager manager = null;
-        try {
-            entityClass = Class.forName(className);
-            // query the entity by the primary key
-            manager = EntityManagerFactory.createEntityManager();
-            entity = (EntityImpl) manager.find(entityClass, primaryKey);
+	@Override
+	protected void init() throws Exception {
+	}
 
-            // look for history data
-            AuditReader auditReader = AuditReaderFactory.get(manager);
-            AuditQuery auditQuery = auditReader.createQuery().forRevisionsOfEntity(entityClass,
-                    false, true);
-            auditQuery = auditQuery.add(AuditEntity.id().eq(primaryKey));
-            auditQuery.addOrder(AuditEntity.revisionNumber().asc());
-            auditQuery.setMaxResults(20);
-            auditQuery.setFirstResult(currentPage);
-            revisionEntries = AuditQueryHelper.listRevisions(auditQuery);
-            computeChanges(revisionEntries);
-        } catch (Exception e) {
-            FacesUtils.addErrorMessage("Änderungshistorie konnte nicht geladen werden");
-        } finally {
-            manager = EntityManagerHelper.close(manager);
-        }
-    }
+	public void queryHistory(ActionEvent event) {
+		Class<?> entityClass;
+		EntityManager manager = null;
+		try {
+			entityClass = Class.forName(className);
+			// query the entity by the primary key
+			manager = EntityManagerFactory.createEntityManager();
+			entity = (EntityImpl) manager.find(entityClass, primaryKey);
 
-    /** Computes the changes for the given revisions */
-    private void computeChanges(List<RevisionInfoEntry> entires) throws Exception {
-        EntityImpl baseEntity = entires.iterator().next().getEntity();
-        for (RevisionInfoEntry entry : entires) {
-            // entry is added, no other compares needed
-            if (entry.getType() == RevisionType.ADD) {
-                continue;
-            }
-            if (entry.getType() == RevisionType.DEL) {
-                continue;
-            }
-            if (entry.getType() == RevisionType.MOD) {
-                // compare each property of the given entity
-                List<RevisionInfoChange> changes = computeChanges(baseEntity, entry.getEntity());
-                for (RevisionInfoChange change : changes) {
-                    entry.addChange(change);
-                }
-                // compare the next entry with this one
-                baseEntity = entry.getEntity();
-            }
-        }
-    }
+			// look for history data
+			AuditReader auditReader = AuditReaderFactory.get(manager);
+			AuditQuery auditQuery = auditReader.createQuery().forRevisionsOfEntity(entityClass,
+					false, true);
+			auditQuery = auditQuery.add(AuditEntity.id().eq(primaryKey));
+			auditQuery.addOrder(AuditEntity.revisionNumber().asc());
+			revisionEntries = AuditQueryHelper.listRevisions(auditQuery);
+			computeChanges(revisionEntries);
+			Collections.reverse(revisionEntries);
+		} catch (Exception e) {
+			FacesUtils.addErrorMessage("Änderungshistorie konnte nicht geladen werden");
+		} finally {
+			manager = EntityManagerHelper.close(manager);
+		}
+	}
 
-    /** Computes the changes between the two entities */
-    @SuppressWarnings("unchecked")
-    private List<RevisionInfoChange> computeChanges(EntityImpl lhsEntity, EntityImpl rhsEntity) throws Exception {
-        List<RevisionInfoChange> changes = new ArrayList<RevisionInfoChange>();
-        // compute the changes and add to the result
-        Map<String, Object> lhsProperties = BeanUtils.describe(lhsEntity);
-        for (String property : lhsProperties.keySet()) {
-            // skip history property
-            if (property.startsWith("history")) {
-                continue;
-            }
-            // skip internal methods from entity
-            if(property.startsWith("displayString")) {
-            	continue;
-            }
-            RevisionInfoChange change = new RevisionInfoChange(property);
+	/** Computes the changes for the given revisions */
+	private void computeChanges(List<RevisionInfoEntry> entires) throws Exception {
+		EntityImpl baseEntity = entires.iterator().next().getEntity();
+		for (RevisionInfoEntry entry : entires) {
+			// entry is added, no other compares needed
+			if (entry.getType() == RevisionType.ADD) {
+				continue;
+			}
+			if (entry.getType() == RevisionType.DEL) {
+				continue;
+			}
+			if (entry.getType() == RevisionType.MOD) {
+				// compare each property of the given entity
+				entry.addChanges(computeChanges(baseEntity, entry.getEntity()));
 
-            // get the value of the property and compute the changes
-            Object lhsProperty = PropertyUtils.getProperty(lhsEntity, property);
-            Object rhsProperty = PropertyUtils.getProperty(rhsEntity, property);
-            if (!computeChanges(change, lhsProperty, rhsProperty)) {
-                continue;
-            }
-            changes.add(change);
-        }
-        return changes;
-    }
+				// compare the next entry with this one
+				baseEntity = entry.getEntity();
+			}
+		}
+	}
 
-    /** Returns whether or not there are changes between the two objects */
-    @SuppressWarnings("unchecked")
-    private boolean computeChanges(RevisionInfoChange change, Object lhs, Object rhs) {
-        // simple cases where one or two sides are not set
-        if (rhs == null && lhs == null) {
-            return false;
-        }
-        // the value is deleted
-        if (rhs == null && lhs != null) {
-            change.setOldValue(getContent(lhs));
-            change.setNewValue(null);
-            return true;
-        }
-        // initial value set
-        if (rhs != null && lhs == null) {
-            change.setNewValue(getContent(rhs));
-            return true;
-        }
+	/** Computes the changes between the two entities */
+	@SuppressWarnings("unchecked")
+	private List<RevisionInfoChange> computeChanges(EntityImpl lhsEntity, EntityImpl rhsEntity) throws Exception {
+		List<RevisionInfoChange> changes = new ArrayList<RevisionInfoChange>();
+		// compute the changes and add to the result
+		Map<String, Object> properties = BeanUtils.describe(lhsEntity);
+		for (String property : properties.keySet()) {
+			// skip history property
+			if (property.startsWith("history")) {
+				continue;
+			}
+			// skip internal methods from entity
+			if (property.startsWith("displayString")) {
+				continue;
+			}
+			RevisionInfoChange change = new RevisionInfoChange(property);
 
-        // compare entities
-        if (rhs instanceof EntityImpl && lhs instanceof EntityImpl) {
-            EntityImpl rhsEntity = (EntityImpl) rhs;
-            EntityImpl lhsEntity = (EntityImpl) lhs;
-            if (rhsEntity.getDisplayString().equals(lhsEntity.getDisplayString())) {
-                return false;
-            }
-            change.setNewValue(getContent(rhsEntity));
-            change.setOldValue(getContent(lhsEntity));
-            return true;
-        }
+			// get the value of the property and compute the changes
+			Object lhsProperty = PropertyUtils.getProperty(lhsEntity, property);
+			Object rhsProperty = PropertyUtils.getProperty(rhsEntity, property);
+			if (!computeChanges(change, lhsProperty, rhsProperty)) {
+				continue;
+			}
+			changes.add(change);
+		}
+		return changes;
+	}
 
-        // compare objects
-        if (rhs instanceof Comparable<?> && lhs instanceof Comparable<?>) {
-            Comparable<Object> rhsComparable = (Comparable<Object>) rhs;
-            Comparable<Object> lhsComparable = (Comparable<Object>) lhs;
-            if (rhsComparable.compareTo(lhsComparable) == 0) {
-                return false;
-            }
-            change.setNewValue(getContent(rhs));
-            change.setOldValue(getContent(lhs));
-            return true;
-        }
-        return false;
-    }
-    
-    /** Helper method to get the current value of an object a string */
-    private String getContent(Object object) {
-    	if(object instanceof EntityImpl) {
-    		EntityImpl entity = (EntityImpl)object;
-    		return entity.getDisplayString();
-    	}
-    	return String.valueOf(object);
-    }
-    
+	/** Returns whether or not there are changes between the two objects */
+	@SuppressWarnings("unchecked")
+	private boolean computeChanges(RevisionInfoChange change, Object lhs, Object rhs) {
+		// simple cases where one or two sides are not set
+		if (rhs == null && lhs == null) {
+			return false;
+		}
+		// the value is deleted
+		if (rhs == null && lhs != null) {
+			change.setOldValue(getContent(lhs));
+			change.setNewValue(null);
+			return true;
+		}
+		// initial value set, ignore (null) to empty string
+		String content = getContent(rhs);
+		if (rhs != null && lhs == null) {
+			if (content.trim().isEmpty()) {
+				return false;
+			}
+			change.setNewValue(content);
+			return true;
+		}
 
-    // ---------------------------------
-    // Setters for the properties
-    // ---------------------------------
-    public void setPrimaryKey(Long primaryKey) {
-        this.primaryKey = primaryKey;
-    }
+		// compare entities
+		if (rhs instanceof EntityImpl && lhs instanceof EntityImpl) {
+			EntityImpl rhsEntity = (EntityImpl) rhs;
+			EntityImpl lhsEntity = (EntityImpl) lhs;
+			if (rhsEntity.getDisplayString().equals(lhsEntity.getDisplayString())) {
+				return false;
+			}
+			change.setNewValue(getContent(rhsEntity));
+			change.setOldValue(getContent(lhsEntity));
+			return true;
+		}
 
-    public void setClassName(String className) {
-        this.className = className;
-    }
+		// compare objects
+		if (rhs instanceof Comparable<?> && lhs instanceof Comparable<?>) {
+			Comparable<Object> rhsComparable = (Comparable<Object>) rhs;
+			Comparable<Object> lhsComparable = (Comparable<Object>) lhs;
+			if (rhsComparable.compareTo(lhsComparable) == 0) {
+				return false;
+			}
+			change.setNewValue(content);
+			change.setOldValue(getContent(lhs));
+			return true;
+		}
+		return false;
+	}
 
-    public void setCurrentPage(Integer currentPage) {
-        this.currentPage = currentPage;
-    }
+	/** Helper method to get the current value of an object as formatted string */
+	private String getContent(Object object) {
+		if (object instanceof EntityImpl) {
+			EntityImpl entity = (EntityImpl) object;
+			return entity.getDisplayString();
+		}
+		if (object instanceof Date) {
+			Date date = (Date) object;
+			return sdf.format(date);
+		}
+		return String.valueOf(object);
+	}
 
-    // ---------------------------------
-    // Getters for the properties
-    // ---------------------------------
-    public Long getPrimaryKey() {
-        return primaryKey;
-    }
+	// ---------------------------------
+	// Setters for the properties
+	// ---------------------------------
+	public void setPrimaryKey(Long primaryKey) {
+		this.primaryKey = primaryKey;
+	}
 
-    public String getClassName() {
-        return className;
-    }
+	public void setClassName(String className) {
+		this.className = className;
+	}
 
-    public Integer getCurrentPage() {
-        return currentPage;
-    }
+	// ---------------------------------
+	// Getters for the properties
+	// ---------------------------------
+	public Long getPrimaryKey() {
+		return primaryKey;
+	}
 
-    public EntityImpl getEntity() {
-        return entity;
-    }
+	public String getClassName() {
+		return className;
+	}
 
-    public List<RevisionInfoEntry> getRevisionEntries() {
-        return revisionEntries;
-    }
+	public EntityImpl getEntity() {
+		return entity;
+	}
+
+	public List<RevisionInfoEntry> getRevisionEntries() {
+		return revisionEntries;
+	}
 }
--- source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/UserOverviewBean.java
+++ source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/UserOverviewBean.java
@@ -17,6 +17,8 @@ import at.redcross.tacos.dbal.entity.SystemUser;
 import at.redcross.tacos.dbal.helper.LocationHelper;
 import at.redcross.tacos.dbal.helper.SystemUserHelper;
 import at.redcross.tacos.dbal.manager.EntityManagerHelper;
+import at.redcross.tacos.web.beans.dto.DtoHelper;
+import at.redcross.tacos.web.beans.dto.GenericDto;
 import at.redcross.tacos.web.faces.FacesUtils;
 import at.redcross.tacos.web.model.SelectableItem;
 import at.redcross.tacos.web.persistence.EntityManagerFactory;
@@ -25,85 +27,86 @@ import at.redcross.tacos.web.persistence.EntityManagerFactory;
 @ManagedBean(name = "userOverviewBean")
 public class UserOverviewBean extends PagingBean {
 
-    private static final long serialVersionUID = -5114023802685654841L;
+	private static final long serialVersionUID = -5114023802685654841L;
 
-    /** available locations */
-    private List<Location> locations;
+	/** available locations */
+	private List<Location> locations;
 
-    /** filter by user status */
-    private boolean locked;
+	/** filter by user status */
+	private boolean locked;
 
-    /** the values for the drop down fields */
-    private List<SelectItem> stateItems;
+	/** the values for the drop down fields */
+	private List<SelectItem> stateItems;
 
-    /** active location */
-    private String locationName = "*";
+	/** active location */
+	private String locationName = "*";
 
-    /** queried results for visualization / reporting */
-    private List<SystemUser> users;
+	/** queried results for visualization / reporting */
+	private List<GenericDto<SystemUser>> users;
 
-    @Override
-    protected void init() throws Exception {
-        EntityManager manager = null;
-        try {
-            manager = EntityManagerFactory.createEntityManager();
-            locations = LocationHelper.list(manager);
-            stateItems = new ArrayList<SelectItem>();
-            stateItems.add(new SelectableItem("nicht gesperrt", false).getItem());
-            stateItems.add(new SelectableItem("gesperrt", true).getItem());
-            // load not locked users
-            loadfromDatabase(manager, getLocationByName(locationName), locked);
-        } finally {
-            manager = EntityManagerHelper.close(manager);
-        }
-    }
+	@Override
+	protected void init() throws Exception {
+		EntityManager manager = null;
+		try {
+			manager = EntityManagerFactory.createEntityManager();
+			locations = LocationHelper.list(manager);
+			stateItems = new ArrayList<SelectItem>();
+			stateItems.add(new SelectableItem("nicht gesperrt", false).getItem());
+			stateItems.add(new SelectableItem("gesperrt", true).getItem());
+			// load not locked users
+			loadfromDatabase(manager, getLocationByName(locationName), locked);
+		} finally {
+			manager = EntityManagerHelper.close(manager);
+		}
+	}
 
-    // ---------------------------------
-    // Actions
-    // ---------------------------------
-    public void tabChanged(ValueChangeEvent event) {
-        EntityManager manager = null;
-        try {
-            page = 1;
-            manager = EntityManagerFactory.createEntityManager();
-            loadfromDatabase(manager, getLocationByName(locationName), locked);
-        } finally {
-            manager = EntityManagerHelper.close(manager);
-        }
-    }
+	// ---------------------------------
+	// Actions
+	// ---------------------------------
+	public void tabChanged(ValueChangeEvent event) {
+		EntityManager manager = null;
+		try {
+			page = 1;
+			manager = EntityManagerFactory.createEntityManager();
+			loadfromDatabase(manager, getLocationByName(locationName), locked);
+		} finally {
+			manager = EntityManagerHelper.close(manager);
+		}
+	}
 
-    public void filterChanged(ActionEvent event) {
-        EntityManager manager = null;
-        try {
-            manager = EntityManagerFactory.createEntityManager();
-            loadfromDatabase(manager, getLocationByName(locationName), locked);
-        } finally {
-            manager = EntityManagerHelper.close(manager);
-        }
-    }
+	public void filterChanged(ActionEvent event) {
+		EntityManager manager = null;
+		try {
+			manager = EntityManagerFactory.createEntityManager();
+			loadfromDatabase(manager, getLocationByName(locationName), locked);
+		} finally {
+			manager = EntityManagerHelper.close(manager);
+		}
+	}
 
-    // ---------------------------------
-    // Private API
-    // ---------------------------------
-    private void loadfromDatabase(EntityManager manager, String locationId, boolean locked) {
-        users = SystemUserHelper.listByLocationName(manager, locationId, locked);
-    }
+	// ---------------------------------
+	// Private API
+	// ---------------------------------
+	private void loadfromDatabase(EntityManager manager, String locationId, boolean locked) {
+		users = DtoHelper.fromList(SystemUser.class, SystemUserHelper.listByLocationName(manager,
+				locationId, locked));
+	}
 
-    private String getLocationByName(String locationName) {
-        if (locationName == null || "*".equals(locationName)) {
-            return null;
-        }
-        for (Location location : locations) {
-            if (location.getName().equals(locationName)) {
-                return location.getName();
-            }
-        }
-        return null;
-    }
-    
-    /**
-	 * Returns whether or not the current authenticated user can edit (delete or change) a user
-	 * entry. The following restrictions are considered:
+	private String getLocationByName(String locationName) {
+		if (locationName == null || "*".equals(locationName)) {
+			return null;
+		}
+		for (Location location : locations) {
+			if (location.getName().equals(locationName)) {
+				return location.getName();
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Returns whether or not the current authenticated user can edit (delete or
+	 * change) a user entry. The following restrictions are considered:
 	 * <ul>
 	 * <li>Principal must have the permission to edit a role</li>
 	 * </ul>
@@ -117,37 +120,37 @@ public class UserOverviewBean extends PagingBean {
 		return false;
 	}
 
-    // ---------------------------------
-    // Setters for the properties
-    // ---------------------------------
-    public void setLocationName(String locationName) {
-        this.locationName = locationName;
-    }
+	// ---------------------------------
+	// Setters for the properties
+	// ---------------------------------
+	public void setLocationName(String locationName) {
+		this.locationName = locationName;
+	}
 
-    public void setLocked(boolean locked) {
-        this.locked = locked;
-    }
+	public void setLocked(boolean locked) {
+		this.locked = locked;
+	}
 
-    // ---------------------------------
-    // Getters for the properties
-    // ---------------------------------
-    public List<Location> getLocations() {
-        return locations;
-    }
+	// ---------------------------------
+	// Getters for the properties
+	// ---------------------------------
+	public List<Location> getLocations() {
+		return locations;
+	}
 
-    public List<SystemUser> getUsers() {
-        return users;
-    }
+	public List<GenericDto<SystemUser>> getUsers() {
+		return users;
+	}
 
-    public String getLocationName() {
-        return locationName;
-    }
+	public String getLocationName() {
+		return locationName;
+	}
 
-    public boolean getLocked() {
-        return locked;
-    }
+	public boolean getLocked() {
+		return locked;
+	}
 
-    public List<SelectItem> getStateItems() {
-        return stateItems;
-    }
+	public List<SelectItem> getStateItems() {
+		return stateItems;
+	}
 }
