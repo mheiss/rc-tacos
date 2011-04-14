6eeeaaf0b3c95910c083cef3fa0a7594d8d73d0e
heissm <heissm@2d6a8bb0-633d-0410-9e00-b961282e9fa3>
2011-04-14 21:30:05 +0000

    Highlighting for Equipment added

--- source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/EquipmentMaintenanceBean.java
+++ source/trunk/at.redcross.tacos.web/src/at/redcross/tacos/web/beans/EquipmentMaintenanceBean.java
@@ -17,6 +17,7 @@ import at.redcross.tacos.dbal.helper.EquipmentHelper;
 import at.redcross.tacos.dbal.manager.EntityManagerHelper;
 import at.redcross.tacos.web.beans.dto.DtoHelper;
 import at.redcross.tacos.web.beans.dto.EntryState;
+import at.redcross.tacos.web.beans.dto.EquipmentDto;
 import at.redcross.tacos.web.beans.dto.GenericDto;
 import at.redcross.tacos.web.faces.FacesUtils;
 import at.redcross.tacos.web.persistence.EntityManagerFactory;
@@ -25,94 +26,96 @@ import at.redcross.tacos.web.persistence.EntityManagerFactory;
 @ManagedBean(name = "equipmentMaintenanceBean")
 public class EquipmentMaintenanceBean extends BaseBean {
 
-	private static final long serialVersionUID = 9185867004047296041L;
+    private static final long serialVersionUID = 9185867004047296041L;
 
-	private final static Log logger = LogFactory.getLog(EquipmentMaintenanceBean.class);
+    private final static Log logger = LogFactory.getLog(EquipmentMaintenanceBean.class);
 
-	/** the available equipments */
-	private List<GenericDto<Equipment>> equipmentEntries;
+    /** the available equipments */
+    private List<EquipmentDto> equipmentEntries;
 
-	/** the id of the selected equipments */
-	private long equipmentId;
+    /** the id of the selected equipments */
+    private long equipmentId;
 
-	@Override
-	protected void init() throws Exception {
-		EntityManager manager = null;
-		try {
-			manager = EntityManagerFactory.createEntityManager();
-			equipmentEntries = DtoHelper.fromList(Equipment.class, EquipmentHelper.list(manager));
-		} finally {
-			manager = EntityManagerHelper.close(manager);
-		}
-	}
+    @Override
+    protected void init() throws Exception {
+        EntityManager manager = null;
+        try {
+            manager = EntityManagerFactory.createEntityManager();
+            equipmentEntries = EquipmentDto.fromList(EquipmentHelper.list(manager));
+        } finally {
+            manager = EntityManagerHelper.close(manager);
+        }
+    }
 
-	// ---------------------------------
-	// Business methods
-	// ---------------------------------
+    // ---------------------------------
+    // Business methods
+    // ---------------------------------
 
-	public void removeEquipment(ActionEvent event) {
-		Iterator<GenericDto<Equipment>> iter = equipmentEntries.iterator();
-		while (iter.hasNext()) {
-			GenericDto<Equipment> dto = iter.next();
-			Equipment equipment = dto.getEntity();
-			if (equipment.getId() != equipmentId) {
-				continue;
-			}
-			if (dto.getState() == EntryState.NEW) {
-				iter.remove();
-			}
+    public void removeEquipment(ActionEvent event) {
+        Iterator<EquipmentDto> iter = equipmentEntries.iterator();
+        while (iter.hasNext()) {
+            EquipmentDto dto = iter.next();
+            Equipment equipment = dto.getEntity();
+            if (equipment.getId() != equipmentId) {
+                continue;
+            }
+            if (dto.getState() == EntryState.NEW) {
+                iter.remove();
+            }
 
-			dto.setState(EntryState.DELETE);
-		}
-	}
+            dto.setState(EntryState.DELETE);
+        }
+    }
 
-	public void unremoveEquipment(ActionEvent event) {
-		for (GenericDto<Equipment> dto : equipmentEntries) {
-			Equipment equipment = dto.getEntity();
-			if (equipment.getId() != equipmentId) {
-				continue;
-			}
-			dto.setState(EntryState.SYNC);
-		}
-	}
+    public void unremoveEquipment(ActionEvent event) {
+        for (GenericDto<Equipment> dto : equipmentEntries) {
+            Equipment equipment = dto.getEntity();
+            if (equipment.getId() != equipmentId) {
+                continue;
+            }
+            dto.setState(EntryState.SYNC);
+        }
+    }
 
-	public void addEquipment(ActionEvent event) {
-		GenericDto<Equipment> dto = new GenericDto<Equipment>(new Equipment());
-		dto.setState(EntryState.NEW);
-		equipmentEntries.add(dto);
-	}
+    public void addEquipment(ActionEvent event) {
+        EquipmentDto dto = new EquipmentDto(new Equipment());
+        dto.setState(EntryState.NEW);
+        equipmentEntries.add(dto);
+    }
 
-	public void saveEquipmentEntries() {
-		EntityManager manager = null;
-		try {
-			manager = EntityManagerFactory.createEntityManager();
-			DtoHelper.syncronize(manager, equipmentEntries);
-			EntityManagerHelper.commit(manager);
-			DtoHelper.filter(equipmentEntries);
-		} catch (Exception ex) {
-			logger.error("Failed to save equipment '" + equipmentId + "'", ex);
-			FacesUtils.addErrorMessage("Die Änderungen konnten nicht gespeichert werden");
-		} finally {
-			EntityManagerHelper.close(manager);
-		}
-	}
-	
+    public void saveEquipmentEntries() {
+        EntityManager manager = null;
+        try {
+            manager = EntityManagerFactory.createEntityManager();
+            DtoHelper.syncronize(manager, equipmentEntries);
+            EntityManagerHelper.commit(manager);
+            DtoHelper.filter(equipmentEntries);
+            for (EquipmentDto dto : equipmentEntries) {
+                dto.refresh();
+            }
+        } catch (Exception ex) {
+            logger.error("Failed to save equipment '" + equipmentId + "'", ex);
+            FacesUtils.addErrorMessage("Die Änderungen konnten nicht gespeichert werden");
+        } finally {
+            EntityManagerHelper.close(manager);
+        }
+    }
 
-	// ---------------------------------
-	// Setters for the properties
-	// ---------------------------------
-	public void setEquipmentId(long equipmentId) {
-		this.equipmentId = equipmentId;
-	}
+    // ---------------------------------
+    // Setters for the properties
+    // ---------------------------------
+    public void setEquipmentId(long equipmentId) {
+        this.equipmentId = equipmentId;
+    }
 
-	// ---------------------------------
-	// Getters for the properties
-	// ---------------------------------
-	public long getEquipmentId() {
-		return equipmentId;
-	}
+    // ---------------------------------
+    // Getters for the properties
+    // ---------------------------------
+    public long getEquipmentId() {
+        return equipmentId;
+    }
 
-	public List<GenericDto<Equipment>> getEquipmentEntries() {
-		return equipmentEntries;
-	}
+    public List<EquipmentDto> getEquipmentEntries() {
+        return equipmentEntries;
+    }
 }
